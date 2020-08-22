43bbd354977e76856512d79a228c138cdb2cee8e
Leo Barney <leo633395@gmail.com>
2020-08-22 11:59:23 -0700

    implement TextFile

--- AbstractionLayer/AL/FileSystem/TextFile.hpp
+++ AbstractionLayer/AL/FileSystem/TextFile.hpp
@@ -18,25 +18,156 @@ namespace AL::FileSystem
 		using File::File;
 
 		// @throw AL::Exceptions::Exception
-		bool Read(char& value);
+		// @return false if not found
+		bool Open(BitMask<FileOpenModes> mode)
+		{
+			mode.Remove(
+				FileOpenModes::Binary
+			);
+
+			return File::Open(
+				mode
+			);
+		}
 
 		// @throw AL::Exceptions::Exception
-		bool ReadLine(String& value);
+		// @return false if end of file
+		bool ReadAll(String& value)
+		{
+			auto valueLength = static_cast<size_t>(
+				(GetSize() * sizeof(String::Char)) - (GetReadPosition() * sizeof(String::Char))
+			);
+
+			value.SetSize(
+				valueLength + 1
+			);
+
+			if (!File::Read(&value[0], valueLength))
+			{
+
+				return false;
+			}
+
+			return true;
+		}
+
+		// @throw AL::Exceptions::Exception
+		// @return false if end of file
+		bool Read(char& value)
+		{
+			if (!File::Read(&value, sizeof(char)))
+			{
+
+				return false;
+			}
+
+			return true;
+		}
+		// @throw AL::Exceptions::Exception
+		// @return false if end of file
+		bool Read(String& value, size_t length)
+		{
+			value.SetSize(
+				length
+			);
+
+			size_t bytesRead;
+
+			if ((bytesRead = File::Read(&value[0], length)) == 0)
+			{
+
+				return false;
+			}
+
+			return true;
+		}
+
+		// @throw AL::Exceptions::Exception
+		// @return false if end of file
+		bool ReadLine(String& value, size_t chunkSize = 128)
+		{
+			size_t bytesRead;
+			size_t totalBytesRead = 0;
+			
+			String lineChunkBuffer(
+				'\0',
+				chunkSize
+			);
+
+			if (value.GetLength())
+			{
+
+				value.Clear();
+			}
+
+			while (bytesRead = File::Read(&lineChunkBuffer[0], chunkSize * sizeof(String::Char)))
+			{
+				totalBytesRead += bytesRead;
+				lineChunkBuffer[bytesRead / sizeof(String::Char)] = String::END;
+
+				auto endOfLineChunk = lineChunkBuffer.IndexOf(
+					LINE_TERMINATOR[0]
+				);
+
+				if (endOfLineChunk == String::NPOS)
+				{
+					value.Append(
+						lineChunkBuffer
+					);
+
+					continue;
+				}
+
+				value.Append(
+					lineChunkBuffer.SubString(
+						0,
+						endOfLineChunk
+					)
+				);
+
+#if defined(AL_PLATFORM_WINDOWS)
+				if (auto endOfLineChunk2 = lineChunkBuffer.IndexOf(LINE_TERMINATOR[1]); endOfLineChunk2 > endOfLineChunk)
+				{
+
+					endOfLineChunk = endOfLineChunk2;
+				}
+#endif
+
+				if (auto diff = (bytesRead - endOfLineChunk))
+				{
+					SetReadPosition(
+						(GetReadPosition() - diff) + 1
+					);
+				}
+
+				break;
+			}
+
+			return totalBytesRead > 0;
+		}
 
 		// @throw AL::Exceptions::Exception
 		void Write(char value)
 		{
-			auto string = String::Format(
-				"%c",
-				value
-			);
-
-			Write(
-				string
+			File::Write(
+				&value,
+				sizeof(char)
 			);
 		}
 		// @throw AL::Exceptions::Exception
-		void Write(const String& value);
+		void Write(const String& value)
+		{
+			size_t start = 0;
+			size_t length = value.GetLength();
+
+			do
+			{
+				start += File::Write(
+					&value[start],
+					length - start
+				);
+			} while (length - start);
+		}
 
 		// @throw AL::Exceptions::Exception
 		void WriteLine(const String& value)
