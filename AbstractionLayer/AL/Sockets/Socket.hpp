59e21ceebfd7a23f8e542adff1d1a982c972c1a4
Leo Barney <leo633395@gmail.com>
2020-11-04 14:11:19 -0800

    implement

--- AbstractionLayer/AL/Sockets/Socket.hpp
+++ AbstractionLayer/AL/Sockets/Socket.hpp
@@ -17,6 +17,8 @@
 
 #include "IPEndPoint.hpp"
 
+#include "AL/Tasks/TaskThreadPool.hpp"
+
 #include "AL/Exceptions/SocketException.hpp"
 
 namespace AL::Sockets
@@ -48,70 +50,8 @@ namespace AL::Sockets
 #endif
 	};
 
-#if defined(AL_PLATFORM_WINDOWS)
-	class Socket;
-
-	struct SocketAsyncContext;
-
-	typedef Function<void(SocketAsyncContext&)> SocketAsyncCallback;
-
-	struct SocketAsyncContext
-	{
-		WSAOVERLAPPED  O;
-
-		Socket* const  lpSocket;
-
-		WSABUF         Buffer;
-		BitMask<DWORD> Flags = 0;
-
-		SocketAsyncCallback	Callback;
-
-		SocketAsyncContext(Socket* lpSocket, void* lpBuffer, size_t bufferSize, SocketAsyncCallback&& callback)
-			: lpSocket(lpSocket),
-			Callback(Move(callback))
-		{
-			ZeroMemory(&O, sizeof(O));
-
-			Buffer.buf = reinterpret_cast<CHAR*>(lpBuffer);
-			Buffer.len = static_cast<ULONG>(bufferSize);
-		}
-
-		auto GetFlags() const
-		{
-			return static_cast<uint32>(
-				O.Offset
-			);
-		}
-
-		auto GetErrorCode() const
-		{
-			return static_cast<OS::ErrorCode>(
-				O.OffsetHigh
-			);
-		}
-
-		auto GetBufferSize() const
-		{
-			return static_cast<size_t>(
-				Buffer.len
-			);
-		}
-
-		auto GetBytesTransferred() const
-		{
-			return static_cast<size_t>(
-				O.InternalHigh
-			);
-		}
-
-		void SetErrorCode(uint32 value)
-		{
-			O.OffsetHigh = static_cast<DWORD>(
-				value
-			);
-		}
-	};
-#endif
+	typedef Function<void(void* lpBuffer, size_t bytesTransferred, Exceptions::Exception* lpException)> SocketAsyncReadCallback;
+	typedef Function<void(const void* lpBuffer, size_t bytesTransferred, Exceptions::Exception* lpException)> SocketAsyncWriteCallback;
 
 	template<AddressFamilies>
 	struct _Socket_SocketAddress;
@@ -346,6 +286,7 @@ namespace AL::Sockets
 #endif
 
 		bool isOpen = false;
+		bool isBound = false;
 		bool isBlocking = true;
 		bool isConnected = false;
 		bool isListening = false;
@@ -425,6 +366,11 @@ namespace AL::Sockets
 		bool IsOpen() const
 		{
 			return isOpen;
+		}
+
+		bool IsBound() const
+		{
+			return isBound;
 		}
 
 		bool IsBlocking() const
@@ -511,7 +457,33 @@ namespace AL::Sockets
 		}
 
 		// @throw AL::Exceptions::Exception
-		void Open();
+		void Open()
+		{
+			AL_ASSERT(!IsOpen(), "Socket already open");
+
+#if defined(AL_PLATFORM_LINUX)
+			if ((hSocket = socket(static_cast<int>(GetAddressFamily()), static_cast<int>(GetType()), static_cast<int>(GetProtocol()))) == INVALID_SOCKET)
+			{
+
+				throw Exceptions::SocketException(
+					"socket"
+				);
+			}
+#elif defined(AL_PLATFORM_WINDOWS)
+			if ((hSocket = WSASocketW(static_cast<int>(GetAddressFamily()), static_cast<int>(GetType()), static_cast<int>(GetProtocol()), nullptr, 0, 0)) == INVALID_SOCKET)
+			{
+
+				throw Exceptions::SocketException(
+					"WSASocketW"
+				);
+			}
+#endif
+
+			isOpen = true;
+
+			localEndPoint = IPEndPoint();
+			remoteEndPoint = IPEndPoint();
+		}
 
 		void Close()
 		{
@@ -524,6 +496,7 @@ namespace AL::Sockets
 #endif
 
 				isOpen = false;
+				isBound = false;
 				isBlocking = true;
 				isConnected = false;
 				isListening = false;
@@ -547,41 +520,533 @@ namespace AL::Sockets
 		}
 
 		// @throw AL::Exceptions::Exception
-		void Bind(const IPEndPoint& localEP);
+		void Bind(const IPEndPoint& localEP)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(!IsBound(), "Socket already bound");
+
+			AL_ASSERT(GetAddressFamily() == localEP.Address.GetFamily(), "Invalid AddressFamily");
+
+			auto addressFamily = GetAddressFamily();
+
+			if (addressFamily == AddressFamilies::IPv6)
+			{
+				int v6_only = 0;
+
+				if (setsockopt(GetHandle(), IPPROTO_IPV6, IPV6_V6ONLY, reinterpret_cast<const char*>(&v6_only), sizeof(v6_only)) == SOCKET_ERROR)
+				{
+
+					throw Exceptions::SocketException(
+						"setsockopt"
+					);
+				}
+			}
+
+			union
+			{
+				typename _Socket_SocketAddress<AddressFamilies::IPv4>::Type address;
+				typename _Socket_SocketAddress<AddressFamilies::IPv6>::Type address6 = { 0 };
+			};
+
+			int addressSize = 0;
+
+			switch (addressFamily)
+			{
+				case AddressFamilies::IPv4:
+					addressSize = sizeof(address);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetPort(address, localEP.Port);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetFamily(address, AddressFamilies::IPv4);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetAddress(address, localEP.Address.GetAddress());
+					break;
+
+				case AddressFamilies::IPv6:
+					addressSize = sizeof(address6);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetPort(address6, localEP.Port);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetFamily(address6, AddressFamilies::IPv6);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetAddress(address6, localEP.Address.GetAddress6());
+					break;
+			}
+
+			if (bind(GetHandle(), reinterpret_cast<const sockaddr*>(&address), addressSize) == SOCKET_ERROR)
+			{
+
+				throw Exceptions::SocketException(
+					"bind"
+				);
+			}
+
+			switch (addressFamily)
+			{
+				case AddressFamilies::IPv4:
+					localEndPoint = IPEndPoint(
+						_Socket_SocketAddress<AddressFamilies::IPv4>::GetAddress(
+							address
+						),
+						_Socket_SocketAddress<AddressFamilies::IPv4>::GetPort(
+							address
+						)
+					);
+					break;
+
+				case AddressFamilies::IPv6:
+					localEndPoint = IPEndPoint(
+						_Socket_SocketAddress<AddressFamilies::IPv6>::GetAddress(
+							address6
+						),
+						_Socket_SocketAddress<AddressFamilies::IPv6>::GetPort(
+							address6
+						)
+					);
+					break;
+			}
+
+			isBound = true;
+		}
 
 		// @throw AL::Exceptions::Exception
-		void Listen(uint32 backlog = BACKLOG_MAX);
+		void Listen(uint32 backlog = BACKLOG_MAX)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsBound(), "Socket not bound");
+			AL_ASSERT(!IsListening(), "Socket already listening");
+
+			if (listen(GetHandle(), backlog) == SOCKET_ERROR)
+			{
+
+				throw Exceptions::SocketException(
+					"listen"
+				);
+			}
+
+			isListening = true;
+		}
 
 		// @throw AL::Exceptions::Exception
-		bool Accept(Socket& socket) const;
+		bool Accept(Socket& socket) const
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsListening(), "Socket not listening");
+
+			AL_ASSERT(GetType() == socket.GetType(), "Invalid SocketType");
+			AL_ASSERT(GetProtocol() == socket.GetProtocol(), "Invalid SocketProtocol");
+			AL_ASSERT(GetAddressFamily() == socket.GetAddressFamily(), "Invalid AddressFamily");
+
+#if defined(AL_PLATFORM_LINUX)
+			if ((socket.hSocket = accept(GetHandle(), nullptr, nullptr)) == INVALID_SOCKET)
+			{
+				auto lastError = GetLastError();
+
+				if ((lastError != EAGAIN) && (lastError != EWOULDBLOCK))
+				{
+
+					throw Exceptions::SocketException(
+						"accept",
+						lastError
+					);
+				}
+
+				return false;
+			}
+#elif defined(AL_PLATFORM_WINDOWS)
+			if ((socket.hSocket = WSAAccept(GetHandle(), nullptr, nullptr, nullptr, 0)) == INVALID_SOCKET)
+			{
+				auto lastError = GetLastError();
+
+				if (lastError != WSAEWOULDBLOCK)
+				{
+
+					throw Exceptions::SocketException(
+						"WSAAccept",
+						lastError
+					);
+				}
+
+				return false;
+			}
+#endif
+
+			try
+			{
+				switch (GetAddressFamily())
+				{
+					case AddressFamilies::IPv4:
+						GetLocalEndPoint<AddressFamilies::IPv4>(socket.GetHandle(), socket.localEndPoint);
+						GetRemoteEndPoint<AddressFamilies::IPv4>(socket.GetHandle(), socket.remoteEndPoint);
+						break;
+
+					case AddressFamilies::IPv6:
+						GetLocalEndPoint<AddressFamilies::IPv6>(socket.GetHandle(), socket.localEndPoint);
+						GetRemoteEndPoint<AddressFamilies::IPv6>(socket.GetHandle(), socket.remoteEndPoint);
+						break;
+				}
+			}
+			catch (Exceptions::Exception& exception)
+			{
+				socket.localEndPoint = IPEndPoint();
+				socket.remoteEndPoint = IPEndPoint();
+
+#if defined(AL_PLATFORM_LINUX)
+				close(socket.GetHandle());
+#elif defined(AL_PLATFORM_WINDOWS)
+				closesocket(socket.GetHandle());
+#endif
+
+				socket.hSocket = INVALID_SOCKET;
+
+				throw Exceptions::Exception(
+					Move(exception),
+					"Error getting end points"
+				);
+			}
+
+#if defined(AL_PLATFORM_LINUX)
+			try
+			{
+				socket.SetBlocking(
+					IsBlocking()
+				);
+			}
+			catch (Exceptions::Exception& exception)
+			{
+				socket.Close();
+
+				throw Exceptions::Exception(
+					Move(exception),
+					"Error setting client blocking state"
+				);
+			}
+#elif defined(AL_PLATFORM_WINDOWS)
+			socket.isBlocking = IsBlocking();
+#endif
+
+			return true;
+		}
 
 		// @throw AL::Exceptions::Exception
 		// @return false on timeout
-		bool Connect(const IPEndPoint& remoteEP);
+		bool Connect(const IPEndPoint& remoteEP)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+
+			AL_ASSERT(GetAddressFamily() == remoteEP.Address.GetFamily(), "Invalid AddressFamily");
+
+			auto addressFamily = GetAddressFamily();
+
+			union
+			{
+				typename _Socket_SocketAddress<AddressFamilies::IPv4>::Type address;
+				typename _Socket_SocketAddress<AddressFamilies::IPv6>::Type address6 = { 0 };
+			};
+
+			int addressSize = 0;
+
+			switch (addressFamily)
+			{
+				case AddressFamilies::IPv4:
+					addressSize = sizeof(address);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetPort(address, remoteEP.Port);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetFamily(address, AddressFamilies::IPv4);
+					_Socket_SocketAddress<AddressFamilies::IPv4>::SetAddress(address, remoteEP.Address.GetAddress());
+					break;
+
+				case AddressFamilies::IPv6:
+					addressSize = sizeof(address6);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetPort(address6, remoteEP.Port);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetFamily(address6, AddressFamilies::IPv6);
+					_Socket_SocketAddress<AddressFamilies::IPv6>::SetAddress(address6, remoteEP.Address.GetAddress6());
+					break;
+			}
+
+#if defined(AL_PLATFORM_LINUX)
+			if (connect(GetHandle(), reinterpret_cast<const sockaddr*>(&address), addressSize) == SOCKET_ERROR)
+			{
+				auto lastError = GetLastError();
+
+				switch (lastError)
+				{
+					case ETIMEDOUT:
+					case ENETUNREACH:
+					case EHOSTDOWN:
+					case EHOSTUNREACH:
+						return false;
+				}
+
+				throw Exceptions::SocketException(
+					"connect",
+					lastError
+				);
+			}
+#elif defined(AL_PLATFORM_WINDOWS)
+			if (connect(GetHandle(), reinterpret_cast<const sockaddr*>(&address), addressSize) == SOCKET_ERROR)
+			{
+				auto lastError = GetLastError();
+
+				switch (lastError)
+				{
+					case WSAETIMEDOUT:
+					case WSAECONNREFUSED:
+						return false;
+				}
+
+				throw Exceptions::SocketException(
+					"connect",
+					lastError
+				);
+			}
+#endif
+
+			try
+			{
+				switch (GetAddressFamily())
+				{
+					case AddressFamilies::IPv4:
+						GetLocalEndPoint<AddressFamilies::IPv4>(GetHandle(), localEndPoint);
+						GetRemoteEndPoint<AddressFamilies::IPv4>(GetHandle(), remoteEndPoint);
+						break;
+
+					case AddressFamilies::IPv6:
+						GetLocalEndPoint<AddressFamilies::IPv6>(GetHandle(), localEndPoint);
+						GetRemoteEndPoint<AddressFamilies::IPv6>(GetHandle(), remoteEndPoint);
+						break;
+				}
+			}
+			catch (Exceptions::Exception& exception)
+			{
+				Close();
+
+				throw Exceptions::Exception(
+					Move(exception),
+					"Error getting end points"
+				);
+			}
+
+			isConnected = true;
+
+			return true;
+		}
 
 		// @throw AL::Exceptions::Exception
 		// @return -1 if would block
 		// @return 0 on connection closed
 		// @return number of bytes received
-		size_t Read(void* lpBuffer, size_t size);
+		size_t Read(void* lpBuffer, size_t size)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsConnected(), "Socket not connected");
 
-#if defined(AL_PLATFORM_WINDOWS)
-		// @throw AL::Exceptions::Exception
-		// @return false on connection closed
-		bool ReadAsync(void* lpBuffer, size_t size, SocketAsyncCallback&& callback);
+			int bytesRead = recv(
+				GetHandle(),
+				static_cast<char*>(lpBuffer),
+				static_cast<int>(size),
+				0
+			);
+
+			if (bytesRead == SOCKET_ERROR)
+			{
+#if defined(AL_PLATFORM_LINUX)
+				auto errorCode = GetLastError();
+
+				if ((errorCode != EAGAIN) && (errorCode != EWOULDBLOCK))
+				{
+					switch (errorCode)
+					{
+						case ECONNRESET:
+						case EHOSTDOWN:
+						case EHOSTUNREACH:
+							Close();
+							return 0;
+					}
+
+					throw Exceptions::SocketException(
+						"recv",
+						errorCode
+					);
+				}
+
+				bytesRead = -1;
+#elif defined(AL_PLATFORM_WINDOWS)
+				switch (auto errorCode = GetLastError())
+				{
+					case WSAEWOULDBLOCK:
+						bytesRead = -1;
+						break;
+
+					case WSAENETDOWN:
+					case WSAENETRESET:
+					case WSAETIMEDOUT:
+					case WSAECONNRESET:
+					case WSAECONNABORTED:
+						Close();
+						break;
+
+					default:
+						throw Exceptions::SocketException(
+							"recv",
+							errorCode
+						);
+				}
 #endif
+			}
+
+			else if (bytesRead == 0)
+			{
+
+				Close();
+			}
+
+			return static_cast<size_t>(
+				bytesRead
+			);
+		}
 
 		// @throw AL::Exceptions::Exception
 		// @return -1 if would block
 		// @return 0 on connection closed
 		// @return number of bytes transmitted
-		size_t Write(void* lpBuffer, size_t size);
+		size_t Write(const void* lpBuffer, size_t size)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsConnected(), "Socket not connected");
 
-#if defined(AL_PLATFORM_WINDOWS)
+			int bytesSent = send(
+				GetHandle(),
+				static_cast<const char*>(lpBuffer),
+				static_cast<int>(size),
+				0
+			);
+
+			if (bytesSent == SOCKET_ERROR)
+			{
+#if defined(AL_PLATFORM_LINUX)
+				auto errorCode = GetLastError();
+
+				if ((errorCode != EAGAIN) && (errorCode != EWOULDBLOCK))
+				{
+					switch (errorCode)
+					{
+						case ECONNRESET:
+						case EHOSTDOWN:
+						case EHOSTUNREACH:
+							Close();
+							return 0;
+					}
+
+					throw Exceptions::SocketException(
+						"send",
+						errorCode
+					);
+				}
+
+				bytesSent = -1;
+#elif defined(AL_PLATFORM_WINDOWS)
+				switch (auto errorCode = GetLastError())
+				{
+					case WSAEWOULDBLOCK:
+						bytesSent = -1;
+						break;
+
+					case WSAENETDOWN:
+					case WSAENETRESET:
+					case WSAETIMEDOUT:
+					case WSAECONNRESET:
+					case WSAECONNABORTED:
+					case WSAEHOSTUNREACH:
+						Close();
+						bytesSent = 0;
+						break;
+
+					default:
+						throw Exceptions::SocketException(
+							"send",
+							errorCode
+						);
+				}
+#endif
+
+			}
+
+			else if (bytesSent == 0)
+			{
+
+				Close();
+			}
+
+			return static_cast<size_t>(
+				bytesSent
+			);
+		}
+
 		// @throw AL::Exceptions::Exception
 		// @return false on connection closed
-		bool WriteAsync(const void* lpBuffer, size_t size, SocketAsyncCallback&& callback);
-#endif
+		bool ReadAsync(void* lpBuffer, size_t size, SocketAsyncReadCallback&& callback, Tasks::TaskThreadPool& threadPool)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsConnected(), "Socket not connected");
+
+			Tasks::Task task(
+				[this, lpBuffer, size, callback = Move(callback)]()
+				{
+					size_t bytesTransferred;
+
+					try
+					{
+						bytesTransferred = Read(
+							lpBuffer,
+							size
+						);
+
+						callback(lpBuffer, bytesTransferred, nullptr);
+					}
+					catch (Exceptions::Exception& exception)
+					{
+
+						callback(lpBuffer, 0, &exception);
+					}
+				}
+			);
+
+			threadPool.Post(
+				Move(task)
+			);
+
+			return true;
+		}
+
+		// @throw AL::Exceptions::Exception
+		// @return false on connection closed
+		bool WriteAsync(const void* lpBuffer, size_t size, SocketAsyncWriteCallback&& callback, Tasks::TaskThreadPool& threadPool)
+		{
+			AL_ASSERT(IsOpen(), "Socket not open");
+			AL_ASSERT(IsConnected(), "Socket not connected");
+
+			Tasks::Task task(
+				[this, lpBuffer, size, callback = Move(callback)]()
+				{
+					size_t bytesTransferred;
+
+					try
+					{
+						bytesTransferred = Write(
+							lpBuffer,
+							size
+						);
+
+						callback(lpBuffer, bytesTransferred, nullptr);
+					}
+					catch (Exceptions::Exception& exception)
+					{
+
+						callback(lpBuffer, 0, &exception);
+					}
+				}
+			);
+
+			threadPool.Post(
+				Move(task)
+			);
+
+			return true;
+		}
 
 		auto& operator = (Socket&& socket)
 		{
@@ -589,6 +1054,9 @@ namespace AL::Sockets
 
 			isOpen = socket.isOpen;
 			socket.isOpen = false;
+
+			isBound = socket.isBound;
+			socket.isBound = false;
 
 			isBlocking = socket.isBlocking;
 			socket.isBlocking = true;
@@ -618,42 +1086,6 @@ namespace AL::Sockets
 		}
 
 	private:
-		// @throw AL::Exceptions::Exception
-		void GetLocalEndPoint(HSOCKET& socket, IPEndPoint& ep)
-		{
-			switch (GetAddressFamily())
-			{
-				case AddressFamilies::IPv4:
-					GetLocalEndPoint<AddressFamilies::IPv4>(socket, ep);
-					break;
-
-				case AddressFamilies::IPv6:
-					GetLocalEndPoint<AddressFamilies::IPv6>(socket, ep);
-					break;
-
-				default:
-					throw Exceptions::NotImplementedException();
-			}
-		}
-
-		// @throw AL::Exceptions::Exception
-		void GetRemoteEndPoint(HSOCKET& socket, IPEndPoint& ep)
-		{
-			switch (GetAddressFamily())
-			{
-				case AddressFamilies::IPv4:
-					GetRemoteEndPoint<AddressFamilies::IPv4>(socket, ep);
-					break;
-
-				case AddressFamilies::IPv6:
-					GetRemoteEndPoint<AddressFamilies::IPv6>(socket, ep);
-					break;
-
-				default:
-					throw Exceptions::NotImplementedException();
-			}
-		}
-
 		// @throw AL::Exceptions::Exception
 		template<AddressFamilies ADDRESS_FAMILY>
 		static void GetLocalEndPoint(HSOCKET socket, IPEndPoint& ep)
