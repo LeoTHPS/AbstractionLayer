277f9f81f5bd40a823a28b5966789b19bed31866
Leo Barney <leo633395@gmail.com>
2020-11-21 22:22:21 -0800

    disable d2d10 and d2d12 until implemented

--- AbstractionLayer/AL/DirectX/Direct3D.hpp
+++ AbstractionLayer/AL/DirectX/Direct3D.hpp
@@ -7,11 +7,11 @@
 
 namespace AL::DirectX
 {
-#if defined(AL_DEPENDENCY_DIRECT3D_12_VERSION)
+#if false && defined(AL_DEPENDENCY_DIRECT3D_12_VERSION)
 	typedef Direct3D12 Direct3D;
 #elif defined(AL_DEPENDENCY_DIRECT3D_11_VERSION)
 	typedef Direct3D11 Direct3D;
-#elif defined(AL_DEPENDENCY_DIRECT3D_10_VERSION)
+#elif false && defined(AL_DEPENDENCY_DIRECT3D_10_VERSION)
 	typedef Direct3D10 Direct3D;
 #endif
 }
