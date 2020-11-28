c48965fe65e3f2454b06469d61e4890ea3953a5a
Leo Barney <leo633395@gmail.com>
2020-11-27 19:39:45 -0800

    add get/set variable for windows

--- AbstractionLayer/AL/OS/Environment.hpp
+++ AbstractionLayer/AL/OS/Environment.hpp
@@ -3,5 +3,68 @@
 
 namespace AL::OS
 {
-	class Environment;
+	class Environment
+	{
+		Environment() = delete;
+
+	public:
+		// @throw AL::Exceptions::Exception
+		// @return false if not found
+		static bool TryGetVariable(const String& name, String& value)
+		{
+#if defined(AL_PLATFORM_LINUX)
+			throw Exceptions::NotImplementedException();
+#elif defined(AL_PLATFORM_WINDOWS)
+			DWORD bufferSize;
+
+			if ((bufferSize = GetEnvironmentVariableA(name.GetCString(), nullptr, 0)) == 0)
+			{
+				if (GetLastError() == ERROR_ENVVAR_NOT_FOUND)
+				{
+
+					return false;
+				}
+
+				throw Exceptions::SystemException(
+					"GetEnvironmentVariableA"
+				);
+			}
+
+			value.SetSize(
+				bufferSize
+			);
+
+			if ((bufferSize = GetEnvironmentVariableA(name.GetCString(), &value[0], bufferSize)) == 0)
+			{
+				if (GetLastError() == ERROR_ENVVAR_NOT_FOUND)
+				{
+
+					return false;
+				}
+
+				throw Exceptions::SystemException(
+					"GetEnvironmentVariableA"
+				);
+			}
+#endif
+
+			return true;
+		}
+
+		// @throw AL::Exceptions::Exception
+		static void SetVariable(const String& name, const String& value)
+		{
+#if defined(AL_PLATFORM_LINUX)
+			throw Exceptions::NotImplementedException();
+#elif defined(AL_PLATFORM_WINDOWS)
+			if (SetEnvironmentVariableA(name.GetCString(), value.GetCString()) == 0)
+			{
+
+				throw Exceptions::SystemException(
+					"SetEnvironmentVariableA"
+				);
+			}
+#endif
+		}
+	};
 }
