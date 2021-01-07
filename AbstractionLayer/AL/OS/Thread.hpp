5f534a66e95083645ae281fe7729e3a4c02aef10
Leo Barney <leo633395@gmail.com>
2021-01-06 23:37:29 -0800

    refactor to use native thread api instead of std::thread

--- AbstractionLayer/AL/OS/Thread.hpp
+++ AbstractionLayer/AL/OS/Thread.hpp
@@ -3,10 +3,9 @@
 
 #include "Timer.hpp"
 
-#include <thread>
-
 #if defined(AL_PLATFORM_LINUX)
 	#include <unistd.h>
+	#include <pthread.h>
 
 	#include <sys/syscall.h>
 #endif
@@ -15,11 +14,232 @@ namespace AL::OS
 {
 	class Thread
 	{
-		bool isRunning = false;
+		class INativeThread
+		{
+			INativeThread(INativeThread&&) = delete;
+			INativeThread(const INativeThread&) = delete;
 
-		std::thread thread;
+		public:
+			INativeThread()
+			{
+			}
 
-		Thread(Thread&&) = delete;
+			virtual ~INativeThread()
+			{
+			}
+
+			virtual bool IsRunning() const = 0;
+
+			virtual bool IsDetatched() const = 0;
+
+			// @throw AL::Exceptions::Exception
+			virtual void Start() = 0;
+
+			// @throw AL::Exceptions::Exception
+			virtual void Detatch() = 0;
+
+			// @throw AL::Exceptions::Exception
+			virtual void Terminate() = 0;
+
+			// @throw AL::Exceptions::Exception
+			// @return false if maxWaitTime elapsed
+			virtual bool Join(TimeSpan maxWaitTime) = 0;
+		};
+
+		template<typename F>
+		class NativeThread
+			: public INativeThread
+		{
+			bool isRunning = false;
+			bool isDetatched = false;
+
+			F function;
+
+#if defined(AL_PLATFORM_LINUX)
+			pthread_t pthread;
+#elif defined(AL_PLATFORM_WINDOWS)
+			HANDLE hThread;
+#endif
+
+		public:
+			explicit NativeThread(F&& function)
+				: function(
+					Move(function)
+				)
+			{
+			}
+
+			virtual ~NativeThread()
+			{
+			}
+
+			virtual bool IsRunning() const override
+			{
+				return isRunning;
+			}
+
+			virtual bool IsDetatched() const override
+			{
+				return isDetatched;
+			}
+
+			// @throw AL::Exceptions::Exception
+			virtual void Start() override
+			{
+				AL_ASSERT(!IsRunning(), "NativeThread already running");
+
+				auto function_detour = [](void* _lpParam)
+				{
+					auto lpNativeThread = reinterpret_cast<NativeThread*>(
+						_lpParam
+					);
+
+					lpNativeThread->function();
+
+					lpNativeThread->isRunning = false;
+
+#if defined(AL_PLATFORM_LINUX)
+					if (lpNativeThread->IsDetatched())
+					{
+
+						delete lpNativeThread;
+					}
+
+					pthread_exit(NULL);
+
+					return (void*)(nullptr);
+#elif defined(AL_PLATFORM_WINDOWS)
+					CloseHandle(
+						lpNativeThread->hThread
+					);
+
+					if (lpNativeThread->IsDetatched())
+					{
+
+						delete lpNativeThread;
+					}
+
+					return DWORD(0);
+#endif
+				};
+
+#if defined(AL_PLATFORM_LINUX)
+				OS::ErrorCode errorCode;
+
+				if ((errorCode = pthread_create(&pthread, nullptr, function_detour, this)) != 0)
+				{
+
+					throw Exceptions::SystemException(
+						"pthread_create",
+						errorCode
+					);
+				}
+#elif defined(AL_PLATFORM_WINDOWS)
+				if ((hThread = CreateThread(nullptr, 0, function_detour, this, 0, nullptr)) == NULL)
+				{
+
+					throw Exceptions::SystemException(
+						"CreateThread"
+					);
+				}
+#endif
+
+				isRunning = true;
+			}
+
+			// @throw AL::Exceptions::Exception
+			virtual void Detatch() override
+			{
+				AL_ASSERT(IsRunning(), "NativeThread not running");
+
+#if defined(AL_PLATFORM_LINUX)
+				OS::ErrorCode errorCode;
+
+				if ((errorCode = pthread_detach(pthread)) != 0)
+				{
+
+					throw Exceptions::SystemException(
+						"pthread_detach",
+						errorCode
+					);
+				}
+#endif
+
+				isDetatched = true;
+			}
+
+			// @throw AL::Exceptions::Exception
+			virtual void Terminate() override
+			{
+				AL_ASSERT(IsRunning(), "NativeThread not running");
+
+#if defined(AL_PLATFORM_LINUX)
+				OS::ErrorCode errorCode;
+
+				if ((errorCode = pthread_cancel(pthread)) != 0)
+				{
+
+					throw Exceptions::SystemException(
+						"pthread_cancel",
+						errorCode
+					);
+				}
+#elif defined(AL_PLATFORM_WINDOWS)
+				if (TerminateThread(hThread, 0) == 0)
+				{
+
+					throw Exceptions::SystemException(
+						"TerminateThread"
+					);
+				}
+
+				CloseHandle(
+					hThread
+				);
+#endif
+
+				isRunning = false;
+			}
+
+			// @throw AL::Exceptions::Exception
+			// @return false if maxWaitTime elapsed
+			virtual bool Join(TimeSpan maxWaitTime) override
+			{
+				AL_ASSERT(IsRunning(), "NativeThread not running");
+
+				Timer timer;
+
+				while (timer.GetElapsed() < maxWaitTime)
+				{
+#if defined(AL_PLATFORM_LINUX)
+					void* threadRetValue;
+					OS::ErrorCode errorCode;
+
+					if ((errorCode = pthread_join(pthread, &threadRetValue)) != 0)
+					{
+
+						throw Exceptions::SystemException(
+							"pthread_join",
+							errorCode
+						);
+					}
+#elif defined(AL_PLATFORM_WINDOWS)
+					if (WaitForSingleObject(hThread, static_cast<DWORD>((maxWaitTime - timer.GetElapsed()).ToMilliseconds())) == 0)
+					{
+
+						throw Exceptions::SystemException(
+							"WaitForSingleObject"
+						);
+					}
+#endif
+				}
+
+				return !IsRunning();
+			}
+		};
+
+		INativeThread* lpNativeThread;
+
 		Thread(const Thread&) = delete;
 
 	public:
@@ -27,21 +247,32 @@ namespace AL::OS
 		template<typename F>
 		static void StartAndDetach(F&& function)
 		{
+			Thread thread;
+
+			thread.Start(
+				Move(function)
+			);
+
 			try
 			{
-				std::thread thread(
-					Move(function)
-				);
-
-				thread.detach();
+				thread.Detatch();
 			}
-			catch (const std::exception& exception)
+			catch (Exceptions::Exception& exception)
 			{
+				try
+				{
+					thread.Terminate();
+				}
+				catch (Exceptions::Exception& innerException)
+				{
 
-				throw Exceptions::Exception(
-					"Error starting std::thread: %s",
-					exception.what()
-				);
+					throw Exceptions::Exception(
+						Move(exception),
+						Move(innerException)
+					);
+				}
+
+				throw;
 			}
 		}
 		// @throw AL::Exceptions::Exception
@@ -67,16 +298,31 @@ namespace AL::OS
 		}
 
 		Thread()
+			: lpNativeThread(
+				nullptr
+			)
 		{
+		}
+
+		Thread(Thread&& thread)
+			: lpNativeThread(
+				Move(thread.lpNativeThread)
+			)
+		{
+			thread.lpNativeThread = nullptr;
 		}
 
 		virtual ~Thread()
 		{
+			if (lpNativeThread != nullptr)
+			{
+				delete lpNativeThread;
+			}
 		}
 
 		bool IsRunning() const
 		{
-			return isRunning;
+			return (lpNativeThread != nullptr) ? lpNativeThread->IsRunning() : false;
 		}
 
 		// @throw AL::Exceptions::Exception
@@ -85,29 +331,27 @@ namespace AL::OS
 		{
 			AL_ASSERT(!IsRunning(), "Thread already running");
 
-			auto detour = [this, function = Move(function)]()
+			if (lpNativeThread != nullptr)
 			{
-				function();
 
-				isRunning = false;
-			};
+				delete lpNativeThread;
+			}
+
+			lpNativeThread = new NativeThread<F>(
+				Move(function)
+			);
 
 			try
 			{
-				thread = std::thread(
-					Move(detour)
-				);
+				lpNativeThread->Start();
 			}
-			catch (const std::exception& exception)
+			catch (Exceptions::Exception&)
 			{
+				delete lpNativeThread;
+				lpNativeThread = nullptr;
 
-				throw Exceptions::Exception(
-					"Error starting std::thread: %s",
-					exception.what()
-				);
+				throw;
 			}
-
-			isRunning = true;
 		}
 		// @throw AL::Exceptions::Exception
 		void Start(void(*lpFunction)())
@@ -132,34 +376,34 @@ namespace AL::OS
 		}
 		
 		// @throw AL::Exceptions::Exception
+		void Detatch()
+		{
+			AL_ASSERT(IsRunning(), "Thread not running");
+
+			lpNativeThread->Detatch();
+			lpNativeThread = nullptr;
+		}
+
+		// @throw AL::Exceptions::Exception
+		void Terminate()
+		{
+			AL_ASSERT(IsRunning(), "Thread not running");
+
+			lpNativeThread->Terminate();
+
+			delete lpNativeThread;
+			lpNativeThread = nullptr;
+		}
+
+		// @throw AL::Exceptions::Exception
 		// @return false if time elapsed and thread is still running
 		bool Join(TimeSpan maxWaitTime = TimeSpan::Infinite)
 		{
-			Timer timer;
+			AL_ASSERT(IsRunning(), "Thread not running");
 
-			while (timer.GetElapsed() < maxWaitTime)
-			{
-				if (!thread.joinable())
-				{
-					
-					break;
-				}
-
-				try
-				{
-					thread.join();
-				}
-				catch (const std::exception& exception)
-				{
-
-					throw Exceptions::Exception(
-						"%s",
-						exception.what()
-					);
-				}
-			}
-
-			return !IsRunning();
+			return lpNativeThread->Join(
+				maxWaitTime
+			);
 		}
 	};
 
