a7a61b3f1f20aa9cf8ab11fab4dc833a47c9e855
Leo Barney <leo633395@gmail.com>
2021-01-28 06:49:28 -0800

    refactor linux build to be non-blocking and implement matching logic for windows builds (needs testing)

--- AbstractionLayer/AL/GPIO/UARTDevice.hpp
+++ AbstractionLayer/AL/GPIO/UARTDevice.hpp
@@ -11,6 +11,59 @@
 	#include <sys/ioctl.h>
 #endif
 
+#if defined(AL_PLATFORM_LINUX)
+	#define AL_GPIO_UART_DEVICE_SPEED_50      B50
+	#define AL_GPIO_UART_DEVICE_SPEED_75      B75
+	#define AL_GPIO_UART_DEVICE_SPEED_110     B110
+	#define AL_GPIO_UART_DEVICE_SPEED_134     B134
+	#define AL_GPIO_UART_DEVICE_SPEED_150     B150
+	#define AL_GPIO_UART_DEVICE_SPEED_200     B200
+	#define AL_GPIO_UART_DEVICE_SPEED_300     B300
+	#define AL_GPIO_UART_DEVICE_SPEED_600     B600
+	#define AL_GPIO_UART_DEVICE_SPEED_1200    B1200
+	#define AL_GPIO_UART_DEVICE_SPEED_1800    B1800
+	#define AL_GPIO_UART_DEVICE_SPEED_2400    B2400
+	#define AL_GPIO_UART_DEVICE_SPEED_4800    B4800
+	#define AL_GPIO_UART_DEVICE_SPEED_9600    B9600
+	#define AL_GPIO_UART_DEVICE_SPEED_14400   B14400
+	#define AL_GPIO_UART_DEVICE_SPEED_19200   B19200
+	#define AL_GPIO_UART_DEVICE_SPEED_38400   B38400
+	#define AL_GPIO_UART_DEVICE_SPEED_56000   B56000
+	#define AL_GPIO_UART_DEVICE_SPEED_57600   B57600
+	#define AL_GPIO_UART_DEVICE_SPEED_115200  B115200
+	#define AL_GPIO_UART_DEVICE_SPEED_128000  B128000
+	#define AL_GPIO_UART_DEVICE_SPEED_230400  B230400
+	#define AL_GPIO_UART_DEVICE_SPEED_256000  B256000
+	#define AL_GPIO_UART_DEVICE_SPEED_460800  B460800
+	#define AL_GPIO_UART_DEVICE_SPEED_500000  B500000
+	#define AL_GPIO_UART_DEVICE_SPEED_576000  B576000
+	#define AL_GPIO_UART_DEVICE_SPEED_921600  B921600
+	#define AL_GPIO_UART_DEVICE_SPEED_1000000 B1000000
+	#define AL_GPIO_UART_DEVICE_SPEED_1152000 B1152000
+	#define AL_GPIO_UART_DEVICE_SPEED_1500000 B1500000
+	#define AL_GPIO_UART_DEVICE_SPEED_2000000 B2000000
+	#define AL_GPIO_UART_DEVICE_SPEED_2500000 B2500000
+	#define AL_GPIO_UART_DEVICE_SPEED_3000000 B3000000
+	#define AL_GPIO_UART_DEVICE_SPEED_3500000 B3500000
+	#define AL_GPIO_UART_DEVICE_SPEED_4000000 B4000000
+#elif defined(AL_PLATFORM_WINDOWS)
+	#define AL_GPIO_UART_DEVICE_SPEED_110     CBR_110
+	#define AL_GPIO_UART_DEVICE_SPEED_300     CBR_300
+	#define AL_GPIO_UART_DEVICE_SPEED_600     CBR_600
+	#define AL_GPIO_UART_DEVICE_SPEED_1200    CBR_1200
+	#define AL_GPIO_UART_DEVICE_SPEED_2400    CBR_2400
+	#define AL_GPIO_UART_DEVICE_SPEED_4800    CBR_4800
+	#define AL_GPIO_UART_DEVICE_SPEED_9600    CBR_9600
+	#define AL_GPIO_UART_DEVICE_SPEED_14400   CBR_14400
+	#define AL_GPIO_UART_DEVICE_SPEED_19200   CBR_19200
+	#define AL_GPIO_UART_DEVICE_SPEED_38400   CBR_38400
+	#define AL_GPIO_UART_DEVICE_SPEED_56000   CBR_56000
+	#define AL_GPIO_UART_DEVICE_SPEED_57600   CBR_57600
+	#define AL_GPIO_UART_DEVICE_SPEED_115200  CBR_115200
+	#define AL_GPIO_UART_DEVICE_SPEED_128000  CBR_128000
+	#define AL_GPIO_UART_DEVICE_SPEED_256000  CBR_256000
+#endif
+
 namespace AL::GPIO
 {
 #if defined(AL_PLATFORM_LINUX)
@@ -21,15 +74,29 @@ namespace AL::GPIO
 
 	class UARTDevice
 	{
-		int fd;
+#if defined(AL_PLATFORM_LINUX)
+		typedef int HFILE;
+		
+		static constexpr HFILE FILE_HANDLE_NULL = -1;
+#elif defined(AL_PLATFORM_WINDOWS)
+		typedef HANDLE HFILE;
+
+		static constexpr HFILE FILE_HANDLE_NULL = INVALID_HANDLE_VALUE;
+#else
+		typedef void* HFILE;
+
+		static constexpr HFILE FILE_HANDLE_NULL = nullptr;
+#endif
+
+		HFILE hFile;
 		String name;
 		UARTDeviceSpeed speed;
 
 		UARTDevice(const UARTDevice&) = delete;
 
-		UARTDevice(int fd, String&& name, UARTDeviceSpeed speed)
-			: fd(
-				fd
+		UARTDevice(HANDLE hFile, String&& name, UARTDeviceSpeed speed)
+			: hFile(
+				hFile
 			),
 			name(
 				Move(name)
@@ -51,52 +118,94 @@ namespace AL::GPIO
 			if ((fd = open(name.GetCString(), O_RDWR | O_NOCTTY | O_NDELAY)) == -1)
 			{
 
-				throw Exceptions::Exception(
-					Exceptions::SystemException(
-						"open"
-					),
-					"Error opening '%s'",
-					name.GetCString()
+				throw Exceptions::SystemException(
+					"open"
 				);
 			}
 
 			fcntl(fd, F_SETFL, O_RDWR);
 
 			termios options;
-			speed_t baudRate;
-			
-			if (!SpeedToBaud(baudRate, speed))
-			{
-				close(fd);
-				
-				throw Exceptions::Exception(
-					"Invalid UARTDeviceSpeed"
-				);
-			}
-
 			tcgetattr(fd, &options);
 			cfmakeraw(&options);
-			cfsetispeed(&options, baudRate);
-			cfsetospeed(&options, baudRate);
+			cfsetispeed(&options, speed);
+			cfsetospeed(&options, speed);
 
 			options.c_cc[VMIN] = 0;
-			options.c_cc[VTIME] = 50; // 5 seconds (deciseconds)
+			options.c_cc[VTIME] = 0;
 
 			options.c_cflag |= CLOCAL | CREAD;
 			options.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
 			options.c_cflag |= CS8;
 			options.c_cflag &= ~(ICANON | ECHO | ECHOE | ISIG | OPOST);
-			
+
 			tcsetattr(fd, TCSANOW, &options);
 
 			ioctl(fd, TIOCMGET, &status);
 			status |= TIOCM_DTR | TIOCM_RTS;
 			ioctl(fd, TIOCMSET, &status);
 
-			usleep(5000); // 5ms
-
 			device = UARTDevice(
 				fd,
+				Move(name),
+				speed
+			);
+#elif defined(AL_PLATFORM_WINDOWS)
+			HANDLE hFile;
+
+			if ((hFile = CreateFileA(name.GetCString(), GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0)) == INVALID_HANDLE_VALUE)
+			{
+
+				throw Exceptions::SystemException(
+					"CreateFileA"
+				);
+			}
+
+			DCB dcb = { 0 };
+			dcb.DCBlength = sizeof(DCB);
+
+			if (!GetCommState(hFile, &dcb))
+			{
+				CloseHandle(hFile);
+
+				throw Exceptions::SystemException(
+					"GetCommState"
+				);
+			}
+
+			dcb.BaudRate = speed;
+			dcb.ByteSize = 8;
+			dcb.Parity = NOPARITY;
+			dcb.fBinary = TRUE;
+			dcb.StopBits = ONESTOPBIT;
+			
+			if (!SetCommState(hFile, &dcb))
+			{
+				CloseHandle(hFile);
+
+				throw Exceptions::SystemException(
+					"SetCommState"
+				);
+			}
+
+			COMMTIMEOUTS timeouts = { 0 };
+			timeouts.ReadIntervalTimeout = MAXWORD;
+			timeouts.ReadTotalTimeoutConstant = 0;
+			timeouts.ReadTotalTimeoutMultiplier = 0;
+			timeouts.WriteTotalTimeoutConstant = 50;
+			timeouts.WriteTotalTimeoutMultiplier = 10;
+
+			if (!SetCommTimeouts(hFile, &timeouts))
+			{
+				CloseHandle(hFile);
+
+				throw Exceptions::SystemException(
+					"SetCommTimeouts"
+				);
+			}
+
+			device = UARTDevice(
+				hFile,
 				Move(name),
 				speed
 			);
@@ -116,7 +225,7 @@ namespace AL::GPIO
 
 		UARTDevice()
 			: UARTDevice(
-				-1,
+				FILE_HANDLE_NULL,
 				"",
 				0
 			)
@@ -124,8 +233,8 @@ namespace AL::GPIO
 		}
 
 		UARTDevice(UARTDevice&& device)
-			: fd(
-				device.fd
+			: hFile(
+				device.hFile
 			),
 			name(
 				Move(device.name)
@@ -134,12 +243,12 @@ namespace AL::GPIO
 				device.speed
 			)
 		{
-			device.fd = -1;
+			device.hFile = FILE_HANDLE_NULL;
 		}
 
 		UARTDevice(String&& name, UARTDeviceSpeed speed)
-			: fd(
-				-1
+			: hFile(
+				FILE_HANDLE_NULL
 			),
 			name(
 				Move(name)
@@ -157,7 +266,7 @@ namespace AL::GPIO
 
 		bool IsOpen() const
 		{
-			return fd != -1;
+			return hFile != FILE_HANDLE_NULL;
 		}
 
 		auto& GetName() const
@@ -174,14 +283,27 @@ namespace AL::GPIO
 		// @return number of bytes read
 		size_t Read(void* lpBuffer, size_t size)
 		{
+
 #if defined(AL_PLATFORM_LINUX)
 			size_t bytesRead;
 
-			if ((bytesRead = read(fd, lpBuffer, size)) == -1)
+			if ((bytesRead = read(hFile, lpBuffer, size)) == -1)
 			{
 
 				throw Exceptions::SystemException(
 					"read"
+				);
+			}
+
+			return bytesRead;
+#elif defined(AL_PLATFORM_WINDOWS)
+			DWORD bytesRead;
+
+			if (!ReadFile(hFile, lpBuffer, static_cast<DWORD>(size), &bytesRead, nullptr))
+			{
+
+				throw Exceptions::SystemException(
+					"ReadFile"
 				);
 			}
 
@@ -204,6 +326,16 @@ namespace AL::GPIO
 					"write"
 				);
 			}
+#elif defined(AL_PLATFORM_WINDOWS)
+			DWORD bytesWritten;
+
+			if (!WriteFile(hFile, lpBuffer, static_cast<DWORD>(size), &bytesWritten, nullptr))
+			{
+
+				throw Exceptions::SystemException(
+					"WriteFile"
+				);
+			}
 #else
 			throw Exceptions::NotImplementedException();
 #endif
@@ -214,9 +346,12 @@ namespace AL::GPIO
 			if (IsOpen())
 			{
 #if defined(AL_PLATFORM_LINUX)
-				close(fd);
-				fd = -1;
+				close(hFile);
+#elif defined(AL_PLATFORM_WINDOWS)
+				CloseHandle(hFile);
 #endif
+
+				hFile = FILE_HANDLE_NULL;
 			}
 		}
 
@@ -224,64 +359,17 @@ namespace AL::GPIO
 		{
 			Close();
 
-			fd = device.fd;
-			device.fd = -1;
+			hFile = device.hFile;
+			device.hFile = FILE_HANDLE_NULL;
 
 			name = Move(
 				device.name
 			);
 
 			speed = device.speed;
+			device.speed = 0;
 
 			return *this;
 		}
-
-	private:
-#if defined(AL_PLATFORM_LINUX)
-		// @return false on invalid speed
-		static bool SpeedToBaud(speed_t& baud, UARTDeviceSpeed speed)
-		{
-#define AL_SPEED_TO_BAUD_CASE(__value__) case __value__: baud = B##__value__; return true
-
-			switch (speed)
-			{
-				AL_SPEED_TO_BAUD_CASE(0);
-				AL_SPEED_TO_BAUD_CASE(50);
-				AL_SPEED_TO_BAUD_CASE(75);
-				AL_SPEED_TO_BAUD_CASE(110);
-				AL_SPEED_TO_BAUD_CASE(134);
-				AL_SPEED_TO_BAUD_CASE(150);
-				AL_SPEED_TO_BAUD_CASE(200);
-				AL_SPEED_TO_BAUD_CASE(300);
-				AL_SPEED_TO_BAUD_CASE(600);
-				AL_SPEED_TO_BAUD_CASE(1200);
-				AL_SPEED_TO_BAUD_CASE(1800);
-				AL_SPEED_TO_BAUD_CASE(2400);
-				AL_SPEED_TO_BAUD_CASE(4800);
-				AL_SPEED_TO_BAUD_CASE(9600);
-				AL_SPEED_TO_BAUD_CASE(19200);
-				AL_SPEED_TO_BAUD_CASE(38400);
-				AL_SPEED_TO_BAUD_CASE(57600);
-				AL_SPEED_TO_BAUD_CASE(115200);
-				AL_SPEED_TO_BAUD_CASE(230400);
-				AL_SPEED_TO_BAUD_CASE(460800);
-				AL_SPEED_TO_BAUD_CASE(500000);
-				AL_SPEED_TO_BAUD_CASE(576000);
-				AL_SPEED_TO_BAUD_CASE(921600);
-				AL_SPEED_TO_BAUD_CASE(1000000);
-				AL_SPEED_TO_BAUD_CASE(1152000);
-				AL_SPEED_TO_BAUD_CASE(1500000);
-				AL_SPEED_TO_BAUD_CASE(2000000);
-				AL_SPEED_TO_BAUD_CASE(2500000);
-				AL_SPEED_TO_BAUD_CASE(3000000);
-				AL_SPEED_TO_BAUD_CASE(3500000);
-				AL_SPEED_TO_BAUD_CASE(4000000);
-			}
-
-#undef AL_SPEED_TO_BAUD_CASE
-
-			return false;
-		}
-#endif
 	};
 }
