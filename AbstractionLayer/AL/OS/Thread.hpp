e502f7ad51ae57e4b728503f7956dded94cfb9e9
Leo Barney <leo633395@gmail.com>
2021-05-16 22:28:53 -0700

    fix

--- AbstractionLayer/AL/OS/Thread.hpp
+++ AbstractionLayer/AL/OS/Thread.hpp
@@ -88,7 +88,7 @@ namespace AL::OS
 			virtual void* GetHandle() const override
 			{
 #if defined(AL_PLATFORM_LINUX)
-				return pthread;
+				return &pthread;
 #elif defined(AL_PLATFORM_WINDOWS)
 				return hThread;
 #endif
