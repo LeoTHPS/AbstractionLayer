f5b07515f918dd37d6494257a8af006d5ef0f9da
Leo Barney <leo633395@gmail.com>
2021-06-01 23:14:51 -0700

    wrap native implementation

--- AbstractionLayer.DotNET/AL/DotNET/Algorithms/Sha1.hpp
+++ AbstractionLayer.DotNET/AL/DotNET/Algorithms/Sha1.hpp
@@ -1,146 +1,102 @@
 #pragma once
 #include "AL/DotNET/Common.hpp"
 
+#include <AL/Algorithms/Sha1.hpp>
+
+#include <AL/Collections/Array.hpp>
+
 namespace AL::DotNET::Algorithms
 {
 	public ref class Sha1
 	{
-		const int shaSuccess      = 0;
-		const int shaNull         = 1;
-		const int shaInputTooLong = 2;
-		const int shaStateError   = 3;
-
-		int computed;
-		int corrupted;
-		::System::UInt32 lengthLow;
-		::System::UInt32 lengthHigh;
-		array<::System::Byte>^ messageBlock;
-		uint16 messageBlockIndex;
-		array<::System::UInt32>^ intermediateHash;
+		AL::Algorithms::Sha1* const lpSha1;
 
 	public:
 		Sha1()
-			: messageBlock(
-				gcnew array<::System::Byte>(64)
-			),
-			intermediateHash(
-				gcnew array<::System::UInt32>(5)
+			: lpSha1(
+				new AL::Algorithms::Sha1()
 			)
 		{
 			Reset();
 		}
 
+		virtual ~Sha1()
+		{
+			delete lpSha1;
+		}
+
 		void Reset()
 		{
-			computed = 0;
-			corrupted = 0;
-
-			lengthLow = 0;
-			lengthHigh = 0;
-
-			messageBlockIndex = 0;
-
-			intermediateHash[0] = 0x67452301;
-			intermediateHash[1] = 0xEFCDAB89;
-			intermediateHash[2] = 0x98BADCFE;
-			intermediateHash[3] = 0x10325476;
-			intermediateHash[4] = 0xC3D2E1F0;
+			lpSha1->Reset();
 		}
 
 		bool Add(array<::System::Byte>^% buffer, ::System::UInt32 offset, ::System::UInt32 count)
 		{
-			if (computed != 0)
-			{
-				corrupted = shaStateError;
+			AL::Collections::Array<uint8> _buffer(
+				count
+			);
 
-				return false;
-			}
+			_buffer.SetSize(
+				count
+			);
 
-			if (corrupted != 0)
-			{
+			Marshal::Copy(
+				buffer,
+				&_buffer[0],
+				offset,
+				count
+			);
 
-				return false;
-			}
-
-			for (::System::UInt32 i = offset, size = buffer->Length; (size-- != 0) && (corrupted == 0); ++i)
-			{
-				messageBlock[messageBlockIndex++] = (byte)(buffer[i] & 0xFF);
-
-				lengthLow += 8;
-
-				if (lengthLow == 0)
-				{
-					lengthHigh++;
-
-					if (lengthHigh == 0)
-					{
-
-						corrupted = 1;
-					}
-				}
-
-				if (messageBlockIndex == 64)
-				{
-
-					ProcessMessageBlock();
-				}
-			}
-
-			return true;
+			return lpSha1->Add(
+				&_buffer[0],
+				_buffer.GetCapacity()
+			);
 		}
 
 		bool Calculate([::System::Runtime::InteropServices::OutAttribute] ::System::String^% result)
 		{
-			array<::System::Byte>^ buffer;
+			AL::Algorithms::Sha1::Hash hash;
 
-			if (Calculate(buffer))
+			if (!lpSha1->Calculate(hash))
 			{
-				result = ::System::BitConverter::ToString(
-					buffer,
-					0,
-					buffer->Length
-				);
 
-				return true;
+				return false;
 			}
 
-			return false;
+			result = gcnew ::System::String(
+				""
+			);
+
+			for (size_t i = 0; i < hash.GetSize(); ++i)
+			{
+				result += ::System::String::Format(
+					"{0:X2}",
+					hash[i]
+				);
+			}
+
+			return true;
 		}
 		bool Calculate([::System::Runtime::InteropServices::OutAttribute] array<::System::Byte>^% buffer)
 		{
-			int i;
+			AL::Algorithms::Sha1::Hash hash;
 
-			if (corrupted != 0)
+			if (!lpSha1->Calculate(hash))
 			{
 
 				return false;
 			}
 
 			buffer = gcnew array<::System::Byte>(
-				20
+				hash.GetSize()
 			);
 
-			if (computed == 0)
-			{
-				PadMessage();
-
-				for (i = 0; i < 64; ++i)
-				{
-
-					messageBlock[i] = 0;
-				}
-
-				computed = 1;
-				lengthLow = 0;
-				lengthHigh = 0;
-			}
-
-			for (i = 0; i < 20; ++i)
-			{
-				buffer[i] = (byte)(
-					intermediateHash[i >> 2] >> 8 * (3 - (i & 0x03))
-				);
-			}
+			Marshal::Copy(
+				&hash[0],
+				buffer,
+				0,
+				static_cast<::System::UInt32>(hash.GetSize())
+			);
 
 			return true;
 		}
@@ -180,133 +136,6 @@ namespace AL::DotNET::Algorithms
 			}
 
 			return true;
-		}
-
-	private:
-		void PadMessage()
-		{
-			if (messageBlockIndex > 55)
-			{
-				messageBlock[messageBlockIndex++] = 0x80;
-
-				while (messageBlockIndex < 64)
-					messageBlock[messageBlockIndex++] = 0;
-
-				ProcessMessageBlock();
-
-				while (messageBlockIndex < 56)
-					messageBlock[messageBlockIndex++] = 0;
-			}
-			else
-			{
-				messageBlock[messageBlockIndex++] = 0x80;
-
-				while (messageBlockIndex < 56)
-					messageBlock[messageBlockIndex++] = 0;
-			}
-
-			messageBlock[56] = (byte)(lengthHigh >> 24);
-			messageBlock[57] = (byte)(lengthHigh >> 16);
-			messageBlock[58] = (byte)(lengthHigh >> 8);
-			messageBlock[59] = (byte)(lengthHigh);
-			messageBlock[60] = (byte)(lengthLow >> 24);
-			messageBlock[61] = (byte)(lengthLow >> 16);
-			messageBlock[62] = (byte)(lengthLow >> 8);
-			messageBlock[63] = (byte)(lengthLow);
-
-			ProcessMessageBlock();
-		}
-
-		void ProcessMessageBlock()
-		{
-			::System::UInt32 K[4] =
-			{
-				0x5A827999,
-				0x6ED9EBA1,
-				0x8F1BBCDC,
-				0xCA62C1D6
-			};
-
-			int t;
-			::System::UInt32 temp;
-			::System::UInt32 W[80];
-			::System::UInt32 A, B, C, D, E;
-
-			for (t = 0; t < 16; t++)
-			{
-				W[t] = (::System::UInt32)(messageBlock[t * 4] << 24);
-				W[t] |= (::System::UInt32)(messageBlock[t * 4 + 1] << 16);
-				W[t] |= (::System::UInt32)(messageBlock[t * 4 + 2] << 8);
-				W[t] |= (::System::UInt32)(messageBlock[t * 4 + 3]);
-			}
-
-			for (t = 16; t < 80; t++)
-			{
-				W[t] = CircularShift(1, W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16]);
-			}
-
-			A = intermediateHash[0];
-			B = intermediateHash[1];
-			C = intermediateHash[2];
-			D = intermediateHash[3];
-			E = intermediateHash[4];
-
-			for (t = 0; t < 20; t++)
-			{
-				temp = CircularShift(5, A) + ((B & C) | ((~B) & D)) + E + W[t] + K[0];
-
-				E = D;
-				D = C;
-				C = CircularShift(30, B);
-				B = A;
-				A = temp;
-			}
-
-			for (t = 20; t < 40; t++)
-			{
-				temp = CircularShift(5, A) + (B ^ C ^ D) + E + W[t] + K[1];
-
-				E = D;
-				D = C;
-				C = CircularShift(30, B);
-				B = A;
-				A = temp;
-			}
-
-			for (t = 40; t < 60; t++)
-			{
-				temp = CircularShift(5, A) + ((B & C) | (B & D) | (C & D)) + E + W[t] + K[2];
-
-				E = D;
-				D = C;
-				C = CircularShift(30, B);
-				B = A;
-				A = temp;
-			}
-
-			for (t = 60; t < 80; t++)
-			{
-				temp = CircularShift(5, A) + (B ^ C ^ D) + E + W[t] + K[3];
-
-				E = D;
-				D = C;
-				C = CircularShift(30, B);
-				B = A;
-				A = temp;
-			}
-
-			messageBlockIndex = 0;
-
-			intermediateHash[0] += A;
-			intermediateHash[1] += B;
-			intermediateHash[2] += C;
-			intermediateHash[3] += D;
-			intermediateHash[4] += E;
-		}
-
-		static ::System::UInt32 CircularShift(::System::UInt32 bits, ::System::UInt32 word)
-		{
-			return (::System::UInt32)(((int)word << (int)bits) | ((int)word >> (int)(32 - bits)));
 		}
 	};
 }
