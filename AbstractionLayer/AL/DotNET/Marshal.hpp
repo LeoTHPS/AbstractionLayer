058d1e9b5e4ca0b7c6ed3ce1cf4670227d0c4370
Leo Barney <leo633395@gmail.com>
2021-06-04 23:44:30 -0700

    remove Copy

--- AbstractionLayer/AL/DotNET/Marshal.hpp
+++ AbstractionLayer/AL/DotNET/Marshal.hpp
@@ -184,33 +184,6 @@ namespace AL::DotNET
 			throw gcnew ::System::NotImplementedException();
 		}
 
-		static void Copy(const void* source, array<::System::Byte>^% destination, ::System::UInt32 offset, ::System::UInt32 count)
-		{
-			Copy(::System::IntPtr(const_cast<void*>(source)), destination, offset, count);
-		}
-		static void Copy(::System::IntPtr source, array<::System::Byte>^% destination, ::System::UInt32 offset, ::System::UInt32 count)
-		{
-			::System::Runtime::InteropServices::Marshal::Copy(
-				source,
-				destination,
-				static_cast<int>(offset),
-				static_cast<int>(count)
-			);
-		}
-		static void Copy(array<::System::Byte>^ source, void* destination, ::System::UInt32 offset, ::System::UInt32 count)
-		{
-			Copy(source, ::System::IntPtr(destination), offset, count);
-		}
-		static void Copy(array<::System::Byte>^ source, ::System::IntPtr destination, ::System::UInt32 offset, ::System::UInt32 count)
-		{
-			::System::Runtime::InteropServices::Marshal::Copy(
-				source,
-				static_cast<int>(offset),
-				destination,
-				static_cast<int>(count)
-			);
-		}
-
 		generic<typename T>
 		static ::System::UInt32 SizeOf()
 		{
