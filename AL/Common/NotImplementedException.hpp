d20ecfbe5cb8280f6b8bfb75df05e0dc7ac7fb41
Francis Barney <leo633395@gmail.com>
2021-10-13 14:17:57 -0700

    refactor to include function, file and line

--- AL/Common/NotImplementedException.hpp
+++ AL/Common/NotImplementedException.hpp
@@ -1,17 +1,51 @@
 #pragma once
 #include "AL/Common.hpp"
 
+#define NotImplementedException() \
+	_NotImplementedException(__FUNCTION__, __FILE__, __LINE__)
+
 namespace AL
 {
-	class NotImplementedException
+	class _NotImplementedException
 		: public Exception
 	{
+		String function;
+		String file;
+		uint32 line;
+
 	public:
-		NotImplementedException()
+		_NotImplementedException(String&& function, String&& file, uint32 line)
 			: Exception(
-				"Not implemented"
+				"Not implemented [Function: %s, File: %s, Line: %lu]",
+				function.GetCString(),
+				file.GetCString(),
+				line
+			),
+			function(
+				Move(function)
+			),
+			file(
+				Move(file)
+			),
+			line(
+				line
 			)
 		{
+		}
+
+		auto GetLine() const
+		{
+			return line;
+		}
+
+		auto& GetFile() const
+		{
+			return file;
+		}
+
+		auto& GetFunction() const
+		{
+			return function;
 		}
 	};
 }
