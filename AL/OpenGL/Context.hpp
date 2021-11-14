85febf0a5e6ab8547c1fa78c958f130fa6572076
Francis Barney <leo633395@gmail.com>
2021-11-13 19:12:26 -0800

    utilize __has_include()

--- AL/OpenGL/Context.hpp
+++ AL/OpenGL/Context.hpp
@@ -2,11 +2,19 @@
 #include "AL/Common.hpp"
 
 #if defined(AL_PLATFORM_LINUX)
-	#include <GL/gl.h>
-	#include <GL/glu.h>
+	#if __has_include(<GL/gl.h>)
+		#define AL_DEPENDENCY_OPENGL
+
+		#include <GL/gl.h>
+		#include <GL/glu.h>
+	#endif
 #elif defined(AL_PLATFORM_WINDOWS)
-	#include <gl/GL.h>
-	#include <gl/GLU.h>
+	#if __has_include(<gl/GL.h>)
+		#define AL_DEPENDENCY_OPENGL
+
+		#include <gl/GL.h>
+		#include <gl/GLU.h>
+	#endif
 #endif
 
 namespace AL::OpenGL
