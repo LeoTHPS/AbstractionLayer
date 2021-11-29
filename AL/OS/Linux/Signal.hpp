6da19e860fa266ad4836e51f33105c333641e447
Francis Barney <leo633395@gmail.com>
2021-11-29 13:45:56 -0800

    add Post and Raise

--- AL/OS/Linux/Signal.hpp
+++ AL/OS/Linux/Signal.hpp
@@ -5,11 +5,12 @@
 	#error Platform not supported
 #endif
 
+#include "AL/OS/Process.hpp"
 #include "AL/OS/SystemException.hpp"
 
 #include <signal.h>
 
-namespace AL::OS::Linux
+namespace AL::OS
 {
 	typedef Void(SignalHandler)(int signal);
 
@@ -18,6 +19,40 @@ namespace AL::OS::Linux
 		Signal() = delete;
 
 	public:
+		// @throw AL::Exception
+		static Void Raise(int signal)
+		{
+			if (::raise(signal) != 0)
+			{
+
+				throw SystemException(
+					"raise"
+				);
+			}
+		}
+
+		// @throw AL::Exception
+		// @return False if would block
+		static Bool Post(ProcessId& processId, int signal)
+		{
+			if (::sigqueue(static_cast<::pid_t>(processId), signal, ::sigval { }) == -1)
+			{
+				auto errorCode = GetLastError();
+
+				if (errorCode == EAGAIN)
+				{
+
+					return False;
+				}
+
+				throw SystemException(
+					"sigqueue"
+				);
+			}
+
+			return True;
+		}
+
 		// @throw AL::Exception
 		// @return previous handler
 		static SignalHandler* SetHandler(int signal, SignalHandler* lpHandler)
