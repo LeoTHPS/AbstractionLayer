3f06be3ed6cf60c8706dd8cd40b82e7814497cd8
Francis Barney <leo633395@gmail.com>
2022-02-08 18:00:00 -0800

    implement AL::OS::Core1

--- AL/OS/Pico/Core1.hpp
+++ AL/OS/Pico/Core1.hpp
@@ -7,8 +7,130 @@
 
 #include "AL/OS/SystemException.hpp"
 
+#include <pico/multicore.h>
+
 namespace AL::OS::Pico
 {
-	// TODO: implement
-	class Core1;
+	class Core1
+	{
+		inline static Bool isRunning = False;
+
+		Core1() = delete;
+
+	public:
+		static Bool IsRunning()
+		{
+			return isRunning;
+		}
+
+		template<typename ... TArgs>
+		static Bool Start(Void(*lpFunction)(TArgs ...), TArgs ... args)
+		{
+			if (IsRunning())
+			{
+
+				return False;
+			}
+
+			::multicore_launch_core1(
+				&Core1::Main<TArgs ...>
+			);
+
+			Push(
+				lpFunction,
+				args ...
+			);
+
+			return True;
+		}
+
+		static Void Reset()
+		{
+			if (IsRunning())
+			{
+				::multicore_reset_core1();
+
+				isRunning = False;
+			}
+		}
+
+		template<typename ... TArgs>
+		static Void Push(TArgs ... args)
+		{
+			PushAll(
+				args ...
+			);
+		}
+
+		template<typename T>
+		static T    Pop()
+		{
+			T value;
+			Pop(value);
+
+			return value;
+		}
+		template<typename ... TArgs>
+		static Void Pop(TArgs& ... args)
+		{
+			PopAll(
+				args ...
+			);
+		}
+
+		template<typename T>
+		static Bool TryPop(T& arg, std::uint32_t timeout_ms)
+		{
+			if (!::multicore_fifo_pop_timeout_us(timeout_ms * 1000, reinterpret_cast<uint32_t*>(&arg)))
+			{
+
+				return False;
+			}
+
+			return True;
+		}
+
+	private:
+		static constexpr Void PushAll()
+		{
+		}
+		template<typename T, typename ... TArgs>
+		static constexpr Void PushAll(T arg, TArgs ... args)
+		{
+			::multicore_fifo_push_blocking(
+				reinterpret_cast<uint32_t>(arg)
+			);
+
+			PushAll(
+				args ...
+			);
+		}
+
+		static constexpr Void PopAll()
+		{
+		}
+		template<typename T, typename ... TArgs>
+		static constexpr Void PopAll(T& arg, TArgs& ... args)
+		{
+			arg = reinterpret_cast<T>(
+				::multicore_fifo_pop_blocking()
+			);
+
+			PopAll(
+				args ...
+			);
+		}
+
+		template<typename ... TArgs>
+		static constexpr Void Main()
+		{
+			isRunning = True;
+
+			Pop<Void(*)(TArgs ...)>()(
+				Pop<TArgs>() ...
+			);
+
+			isRunning = False;
+		}
+	};
 }
