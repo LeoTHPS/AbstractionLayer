92d6d6cd9fa65e7f9d7471fb323cc949271a0a79
Francis Barney <leo633395@gmail.com>
2022-02-18 20:05:09 -0800

    refactor

--- AL/Common/Math.hpp
+++ AL/Common/Math.hpp
@@ -10,8 +10,7 @@ namespace AL
 		Math() = delete;
 
 	public:
-		static constexpr Double PI = 3.14159265358979323846;
-
+		static constexpr Double PI      = 3.14159265358979323846;
 		static constexpr Double Rad2Deg = 360 / (PI * 2);
 
 		template<typename T>
@@ -52,39 +51,121 @@ namespace AL
 		}
 
 		template<typename T, size_t S>
-		static T Lowest(const T(&values)[S])
+		static           auto Add(const T(&values)[S])
 		{
-			auto lowest = values[0];
+			auto result  = values[0];
 			auto lpValue = &values[1];
 
 			for (size_t i = 1; i < S; ++i, ++lpValue)
 			{
-				if (*lpValue < lowest)
-				{
-
-					lowest = *lpValue;
-				}
+				result += *lpValue;
 			}
 
-			return lowest;
+			return result;
+		}
+		template<typename ... T_VALUES>
+		static constexpr auto Add(T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+
+				return (values + ...);
+			}
 		}
 
 		template<typename T, size_t S>
-		static T Highest(const T(&values)[S])
+		static           auto Subtract(const T(&values)[S])
 		{
-			auto highest = values[0];
+			auto result  = values[0];
 			auto lpValue = &values[1];
 
 			for (size_t i = 1; i < S; ++i, ++lpValue)
 			{
-				if (*lpValue > highest)
+				result -= *lpValue;
+			}
+
+			return result;
+		}
+		template<typename ... T_VALUES>
+		static constexpr auto Subtract(T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return _Subtract<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...
+				);
+			}
+		}
+
+		template<typename T, size_t S>
+		static constexpr auto Average(const T(&values)[S])
+		{
+			return Add<T, S>(values) / S;
+		}
+		template<typename ... T_VALUES>
+		static constexpr auto Average(T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+
+				return Add<T_VALUES ...>(Forward<T_VALUES>(values) ...) / (sizeof ...(T_VALUES));
+			}
+		}
+
+		template<typename T, size_t S>
+		static           auto Lowest(const T(&values)[S])
+		{
+			auto result  = values[0];
+			auto lpValue = &values[1];
+
+			for (size_t i = 1; i < S; ++i, ++lpValue)
+			{
+				if (*lpValue < result)
 				{
 
-					highest = *lpValue;
+					result = *lpValue;
 				}
 			}
 
-			return highest;
+			return result;
+		}
+		template<typename ... T_VALUES>
+		static constexpr auto Lowest(T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return _Lowest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...
+				);
+			}
+		}
+
+		template<typename T, size_t S>
+		static           auto Highest(const T(&values)[S])
+		{
+			auto result  = values[0];
+			auto lpValue = &values[1];
+
+			for (size_t i = 1; i < S; ++i, ++lpValue)
+			{
+				if (*lpValue > result)
+				{
+
+					result = *lpValue;
+				}
+			}
+
+			return result;
+		}
+		template<typename ... T_VALUES>
+		static constexpr auto Highest(T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return _Highest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...
+				);
+			}
 		}
 
 		template<typename T>
@@ -105,9 +186,127 @@ namespace AL
 		}
 
 		template<typename T>
-		static constexpr T Round(T value, T low, T high)
+		static constexpr T Round(T value, T min, T max)
 		{
-			return ((value - low) < (high - value)) ? low : high;
+			return ((value - min) < (max - value)) ? min : max;
+		}
+
+	private:
+		template<typename T_VALUE>
+		static constexpr auto _Subtract(T_VALUE value)
+		{
+			return value;
+		}
+		template<typename T_VALUE_1, typename T_VALUE_2, typename ... T_VALUES>
+		static constexpr auto _Subtract(T_VALUE_1 value1, T_VALUE_2 value2, T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return __Subtract<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					value1 - value2
+				);
+			}
+			else
+			{
+
+				return value1 - value2;
+			}
+		}
+		template<typename T_VALUE, typename ... T_VALUES, typename T_RESULT>
+		static constexpr auto __Subtract(T_VALUE value, T_VALUES ... values, T_RESULT result)
+		{
+			if constexpr (sizeof ...(T_VALUES) == 0)
+			{
+
+				return result - value;
+			}
+			else
+			{
+
+				return __Subtract<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					result - value
+				);
+			}
+		}
+
+		template<typename T_VALUE>
+		static constexpr auto _Lowest(T_VALUE value)
+		{
+			return value;
+		}
+		template<typename T_VALUE_1, typename T_VALUE_2, typename ... T_VALUES>
+		static constexpr auto _Lowest(T_VALUE_1 value1, T_VALUE_2 value2, T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return __Lowest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					(value1 < value2) ? value1 : value2
+				);
+			}
+			else
+			{
+
+				return (value1 < value2) ? value1 : value2;
+			}
+		}
+		template<typename T_VALUE, typename ... T_VALUES, typename T_RESULT>
+		static constexpr auto __Lowest(T_VALUE value, T_VALUES ... values, T_RESULT result)
+		{
+			if constexpr (sizeof ...(T_VALUES) == 0)
+			{
+
+				return (value < result) ? value : result;
+			}
+			else
+			{
+
+				return __Lowest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					(value < result) ? value : result
+				);
+			}
+		}
+
+		template<typename T_VALUE>
+		static constexpr auto _Highest(T_VALUE value)
+		{
+			return value;
+		}
+		template<typename T_VALUE_1, typename T_VALUE_2, typename ... T_VALUES>
+		static constexpr auto _Highest(T_VALUE_1 value1, T_VALUE_2 value2, T_VALUES ... values)
+		{
+			if constexpr (sizeof ...(T_VALUES) > 0)
+			{
+				return __Highest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					(value1 > value2) ? value1 : value2
+				);
+			}
+			else
+			{
+
+				return (value1 > value2) ? value1 : value2;
+			}
+		}
+		template<typename T_VALUE, typename ... T_VALUES, typename T_RESULT>
+		static constexpr auto __Highest(T_VALUE value, T_VALUES ... values, T_RESULT result)
+		{
+			if constexpr (sizeof ...(T_VALUES) == 0)
+			{
+
+				return (value > result) ? value : result;
+			}
+			else
+			{
+
+				return __Highest<T_VALUES ...>(
+					Forward<T_VALUES>(values) ...,
+					(value > result) ? value : result
+				);
+			}
 		}
 	};
 }
