4b039413e7055f68ade3996dd78a663b00629349
Francis Barney <leo633395@gmail.com>
2022-06-20 14:30:38 -0700

    refactor to function similar to NotImplementedException

--- AL/Common/OperationNotSupportedException.hpp
+++ AL/Common/OperationNotSupportedException.hpp
@@ -1,17 +1,51 @@
 #pragma once
 #include "AL/Common.hpp"
 
+#define OperationNotSupportedException() \
+	_OperationNotSupportedException(__FUNCTION__, __FILE__, __LINE__)
+
 namespace AL
 {
-	class OperationNotSupportedException
+	class _OperationNotSupportedException
 		: public Exception
 	{
+		String function;
+		String file;
+		uint32 line;
+
 	public:
-		OperationNotSupportedException()
+		_OperationNotSupportedException(String&& function, String&& file, uint32 line)
 			: Exception(
-				"Operation not supported"
+				"Operation not supported [Function: %s, File: %s, Line: %lu]",
+				function.GetCString(),
+				file.GetCString(),
+				line
+			),
+			function(
+				Move(function)
+			),
+			file(
+				Move(file)
+			),
+			line(
+				line
 			)
 		{
+		}
+
+		auto GetLine() const
+		{
+			return line;
+		}
+
+		auto& GetFile() const
+		{
+			return file;
+		}
+
+		auto& GetFunction() const
+		{
+			return function;
 		}
 	};
 }
