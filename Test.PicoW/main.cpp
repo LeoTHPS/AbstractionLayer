3fd0aafd10be69c220f60eeeefb4b6beac862f3f
Francis Barney <leo633395@gmail.com>
2022-07-15 11:02:12 -0700

    refactor

--- Test.PicoW/main.cpp
+++ Test.PicoW/main.cpp
@@ -14,10 +14,42 @@
 #include <AL/Hardware/PicoW/ADC.hpp>
 #include <AL/Hardware/PicoW/CYW43.hpp>
 
+#define WIFI_SSID      "Leo-Wireless"
+#define WIFI_PASSWORD  ""
+#define WIFI_AUTH_TYPE AL::Hardware::PicoW::CYW43AuthTypes::Open
+
 // @throw AL::Exception
 void do_the_thing()
 {
-	AL::Hardware::PicoW::CYW43::Open();
+	AL::Hardware::PicoW::CYW43::Open(
+		AL::Hardware::PicoW::CYW43Countries::USA
+	);
+
+	AL::Hardware::PicoW::CYW43ScanCallback onScan(
+		[](const AL::Hardware::PicoW::CYW43Network& _network)
+		{
+			AL::OS::Console::WriteLine(
+				"[RSSI: %i, SSID: %s, BSSID: %s, Channel: %u, AuthType: %u]",
+				_network.RSSI,
+				_network.SSID.GetCString(),
+				_network.BSSID.GetCString(),
+				_network.Channel,
+				_network.AuthType
+			);
+
+			return AL::True;
+		}
+	);
+
+	AL::Hardware::PicoW::CYW43::Scan(
+		onScan
+	);
+
+	AL::Hardware::PicoW::CYW43::Connect(
+		WIFI_SSID,
+		WIFI_PASSWORD,
+		WIFI_AUTH_TYPE
+	);
 
 	AL::OS::Timer timer;
 
