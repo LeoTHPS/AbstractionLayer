255d3cad6aaa243162e42790c987079344bd611a
Francis Barney <leo633395@gmail.com>
2022-07-15 08:58:43 -0700

    initialize with LED disabled

--- AL/Hardware/PicoW/CYW43.hpp
+++ AL/Hardware/PicoW/CYW43.hpp
@@ -33,6 +33,63 @@ namespace AL::Hardware::PicoW
 		WPA2_MIXED_PSK = CYW43_AUTH_WPA2_MIXED_PSK
 	};
 
+	enum class CYW43Countries : uint32
+	{
+		Worldwide      = CYW43_COUNTRY_WORLDWIDE,
+
+		Australia      = CYW43_COUNTRY_AUSTRALIA,
+		Austria        = CYW43_COUNTRY_AUSTRIA,
+		Belgium        = CYW43_COUNTRY_BELGIUM,
+		Brazil         = CYW43_COUNTRY_BRAZIL,
+		Canada         = CYW43_COUNTRY_CANADA,
+		Chile          = CYW43_COUNTRY_CHILE,
+		China          = CYW43_COUNTRY_CHINA,
+		Colombia       = CYW43_COUNTRY_COLOMBIA,
+		Czech_Republic = CYW43_COUNTRY_CZECH_REPUBLIC,
+		Denmark        = CYW43_COUNTRY_DENMARK,
+		Estonia        = CYW43_COUNTRY_ESTONIA,
+		Finland        = CYW43_COUNTRY_FINLAND,
+		France         = CYW43_COUNTRY_FRANCE,
+		Germany        = CYW43_COUNTRY_GERMANY,
+		Greece         = CYW43_COUNTRY_GREECE,
+		Hong_Kong      = CYW43_COUNTRY_HONG_KONG,
+		Hungary        = CYW43_COUNTRY_HUNGARY,
+		Iceland        = CYW43_COUNTRY_ICELAND,
+		India          = CYW43_COUNTRY_INDIA,
+		Israel         = CYW43_COUNTRY_ISRAEL,
+		Italy          = CYW43_COUNTRY_ITALY,
+		Japan          = CYW43_COUNTRY_JAPAN,
+		Kenya          = CYW43_COUNTRY_KENYA,
+		Latvia         = CYW43_COUNTRY_LATVIA,
+		Liechtenstein  = CYW43_COUNTRY_LIECHTENSTEIN,
+		Lithuania      = CYW43_COUNTRY_LITHUANIA,
+		Luxembourg     = CYW43_COUNTRY_LUXEMBOURG,
+		Malaysia       = CYW43_COUNTRY_MALAYSIA,
+		Malta          = CYW43_COUNTRY_MALTA,
+		Mexico         = CYW43_COUNTRY_MEXICO,
+		Netherlands    = CYW43_COUNTRY_NETHERLANDS,
+		New_Zealand    = CYW43_COUNTRY_NEW_ZEALAND,
+		Nigeria        = CYW43_COUNTRY_NIGERIA,
+		Norway         = CYW43_COUNTRY_NORWAY,
+		Peru           = CYW43_COUNTRY_PERU,
+		Philippines    = CYW43_COUNTRY_PHILIPPINES,
+		Poland         = CYW43_COUNTRY_POLAND,
+		Portugal       = CYW43_COUNTRY_PORTUGAL,
+		Singapore      = CYW43_COUNTRY_SINGAPORE,
+		Slovakia       = CYW43_COUNTRY_SLOVAKIA,
+		Slovenia       = CYW43_COUNTRY_SLOVENIA,
+		South_Africa   = CYW43_COUNTRY_SOUTH_AFRICA,
+		South_Korea    = CYW43_COUNTRY_SOUTH_KOREA,
+		Spain          = CYW43_COUNTRY_SPAIN,
+		Sweden         = CYW43_COUNTRY_SWEDEN,
+		Switzerland    = CYW43_COUNTRY_SWITZERLAND,
+		Taiwan         = CYW43_COUNTRY_TAIWAN,
+		Thailand       = CYW43_COUNTRY_THAILAND,
+		Turkey         = CYW43_COUNTRY_TURKEY,
+		UK             = CYW43_COUNTRY_UK,
+		USA            = CYW43_COUNTRY_USA
+	};
+
 	class CYW43
 	{
 		inline static Bool isOpen      = False;
@@ -91,7 +148,7 @@ namespace AL::Hardware::PicoW
 		}
 
 		// @throw AL::Exception
-		static Void Open()
+		static Void Open(CYW43Countries country = CYW43Countries::Worldwide)
 		{
 			AL_ASSERT(
 				!IsOpen(),
@@ -99,7 +156,32 @@ namespace AL::Hardware::PicoW
 			);
 
 #if defined(AL_DEPENDENCY_CYW43)
-			::cyw43_arch_init();
+			int errorCode;
+
+			if ((errorCode = ::cyw43_arch_init_with_country(static_cast<typename Get_Enum_Or_Integer_Base<CYW43Countries>::Type>(country))) != 0)
+			{
+
+				throw OS::SystemException(
+					"cyw43_arch_init_with_country",
+					errorCode
+				);
+			}
+
+			try
+			{
+				LED::Write(
+					False
+				);
+			}
+			catch (Exception& exception)
+			{
+				::cyw43_arch_deinit();
+
+				throw Exception(
+					Move(exception),
+					"Error disabling LED"
+				);
+			}
 #else
 			throw DependencyMissingException(
 				"CYW43"
@@ -114,6 +196,16 @@ namespace AL::Hardware::PicoW
 			if (IsOpen())
 			{
 #if defined(AL_DEPENDENCY_CYW43)
+				try
+				{
+					LED::Write(
+						False
+					);
+				}
+				catch (const Exception&)
+				{
+				}
+
 				::cyw43_arch_deinit();
 #endif
 
