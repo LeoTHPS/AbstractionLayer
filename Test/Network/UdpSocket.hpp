673f349e451ae540eeb09d2bda810263517f2c10
Francis Barney <leo633395@gmail.com>
2022-11-24 11:09:23 -0800

    add AL::Network::UdpSocket example

---
+++ Test/Network/UdpSocket.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <AL/Common.hpp>
+
+#include <AL/OS/Console.hpp>
+
+#include <AL/Collections/Array.hpp>
+
+#include <AL/Network/UdpSocket.hpp>
+
+// @throw AL::Exception
+static void AL_Network_UdpSocket()
+{
+	using namespace AL;
+	using namespace AL::Network;
+
+	UdpSocket socket(
+		AddressFamilies::IPv4
+	);
+
+	socket.Open();
+
+	socket.Bind(
+		IPEndPoint
+		{
+			.Host = IPAddress::Any(),
+			.Port = 10000
+		}
+	);
+
+	IPEndPoint remoteEP;
+	AL::size_t numberOfBytesReceived;
+
+	Collections::Array<uint8> buffer(
+		0xFFF
+	);
+
+	socket.Send(
+		&buffer[0],
+		buffer.GetCapacity(),
+		IPEndPoint { .Host = IPAddress::Loopback(), .Port = 10000 }
+	);
+
+	while ((numberOfBytesReceived = socket.Receive(&buffer[0], buffer.GetCapacity(), remoteEP)) != 0)
+	{
+#if defined(AL_TEST_SHOW_CONSOLE_OUTPUT)
+		OS::Console::WriteLine(
+			"Received %s bytes from %s:%u [%s]",
+			ToString(numberOfBytesReceived).GetCString(),
+			remoteEP.Host.ToString().GetCString(),
+			remoteEP.Port,
+			HexConverter::Encode(&buffer[0], numberOfBytesReceived).GetCString()
+		);
+#endif
+	}
+
+	socket.Close();
+}
