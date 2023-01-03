2ac25a2f2019eb490ec0b5cdabc8974bfc871319
Francis Barney <leo633395@gmail.com>
2023-01-02 17:47:18 -0800

    refactor

--- AL/OS/OpenGL/Context.hpp
+++ AL/OS/OpenGL/Context.hpp
@@ -11,37 +11,33 @@
 
 #include "AL/OS/SystemException.hpp"
 
-#if defined(AL_PLATFORM_PICO)
-	#warning Platform not supported
-#elif defined(AL_PLATFORM_LINUX)
-	#if AL_HAS_INCLUDE(<GL/gl.h>)
-		#define AL_DEPENDENCY_OPENGL
-
-		#include <GL/gl.h>
-		#include <GL/glu.h>
+#if defined(AL_PLATFORM_LINUX)
+	#if !AL_HAS_INCLUDE(<GL/gl.h>)
+		#error Missing GL/gl.h
 	#endif
+
+	#include <GL/gl.h>
+	#include <GL/glu.h>
 #elif defined(AL_PLATFORM_WINDOWS)
 	#include "AL/OS/Windows/GDI/GDIException.hpp"
 
-	#if AL_HAS_INCLUDE(<gl/GL.h>)
-		#define AL_DEPENDENCY_OPENGL
+	#if !AL_HAS_INCLUDE(<gl/GL.h>)
+		#error Missing gl/GL.h
+	#endif
 
-		#include <GL/gl.h>
-		#include <GL/glu.h>
-		#include <GL/glaux.h>
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+	#include <GL/glaux.h>
 
-		#if defined(AL_COMPILER_MSVC)
-			#pragma comment(lib, "Gdi32.lib")
-			#pragma comment(lib, "glu32.lib")
-			#pragma comment(lib, "opengl32.lib")
-		#endif
+	#if defined(AL_COMPILER_MSVC)
+		#pragma comment(lib, "Gdi32.lib")
+		#pragma comment(lib, "glu32.lib")
+		#pragma comment(lib, "opengl32.lib")
 	#endif
 
 	#include <wingdi.h>
-#endif
-
-#if !defined(AL_DEPENDENCY_OPENGL)
-	typedef float GLfloat;
+#else
+	#error Platform not supported
 #endif
 
 namespace AL::OS::OpenGL
@@ -56,13 +52,11 @@ namespace AL::OS::OpenGL
 
 		Bool isCreated = False;
 
-#if defined(AL_DEPENDENCY_OPENGL)
-	#if defined(AL_PLATFORM_LINUX)
+#if defined(AL_PLATFORM_LINUX)
 
-	#elif defined(AL_PLATFORM_WINDOWS)
+#elif defined(AL_PLATFORM_WINDOWS)
 		::HDC   hDC;
 		::HGLRC hGLRC;
-	#endif
 #endif
 
 		HWINDOW          hWindow;
@@ -87,12 +81,10 @@ namespace AL::OS::OpenGL
 
 		auto GetHandle() const
 		{
-#if defined(AL_DEPENDENCY_OPENGL)
-	#if defined(AL_PLATFORM_LINUX)
+#if defined(AL_PLATFORM_LINUX)
 			// TODO: implement
-	#elif defined(AL_PLATFORM_WINDOWS)
+#elif defined(AL_PLATFORM_WINDOWS)
 			return hGLRC;
-	#endif
 #endif
 		}
 
@@ -104,11 +96,10 @@ namespace AL::OS::OpenGL
 				"Context already created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
-	#if defined(AL_PLATFORM_LINUX)
+#if defined(AL_PLATFORM_LINUX)
 			// TODO: implement
 			throw NotImplementedException();
-	#elif defined(AL_PLATFORM_WINDOWS)
+#elif defined(AL_PLATFORM_WINDOWS)
 			hDC = ::GetDC(
 				hWindow
 			);
@@ -177,11 +168,6 @@ namespace AL::OS::OpenGL
 					"wglMakeCurrent"
 				);
 			}
-	#endif
-#else
-			throw DependencyMissingException(
-				"OpenGL"
-			);
 #endif
 
 			this->hWindow = hWindow;
@@ -193,10 +179,9 @@ namespace AL::OS::OpenGL
 		{
 			if (IsCreated())
 			{
-#if defined(AL_DEPENDENCY_OPENGL)
-	#if defined(AL_PLATFORM_LINUX)
+#if defined(AL_PLATFORM_LINUX)
 				// TODO: implement
-	#elif defined(AL_PLATFORM_WINDOWS)
+#elif defined(AL_PLATFORM_WINDOWS)
 				::wglMakeCurrent(
 					NULL,
 					NULL
@@ -210,7 +195,6 @@ namespace AL::OS::OpenGL
 					hWindow,
 					hDC
 				);
-	#endif
 #endif
 
 				isCreated = False;
@@ -225,7 +209,6 @@ namespace AL::OS::OpenGL
 				"Context not created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
 			if (width == 0)
 			{
 
@@ -247,9 +230,6 @@ namespace AL::OS::OpenGL
 
 			clientSize.Width  = width & Integer<uint32>::SignedCastMask;
 			clientSize.Height = height & Integer<uint32>::SignedCastMask;
-#else
-			throw NotImplementedException();
-#endif
 		}
 
 		// @throw AL::Exception
@@ -260,7 +240,6 @@ namespace AL::OS::OpenGL
 				"Context not created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
 			::glClearColor(
 				color.R / 255.0f,
 				color.G / 255.0f,
@@ -273,9 +252,6 @@ namespace AL::OS::OpenGL
 			);
 
 			::glLoadIdentity();
-#else
-			throw NotImplementedException();
-#endif
 		}
 
 		// @throw AL::Exception
@@ -286,12 +262,11 @@ namespace AL::OS::OpenGL
 				"Context not created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
-	#if defined(AL_PLATFORM_LINUX)
+#if defined(AL_PLATFORM_LINUX)
 			// TODO: vsync
 			// TODO: implement
 			throw NotImplementedException();
-	#elif defined(AL_PLATFORM_WINDOWS)
+#elif defined(AL_PLATFORM_WINDOWS)
 			if (!::SwapBuffers(hDC))
 			{
 
@@ -299,9 +274,6 @@ namespace AL::OS::OpenGL
 					"SwapBuffers"
 				);
 			}
-	#endif
-#else
-			throw NotImplementedException();
 #endif
 		}
 
@@ -341,7 +313,6 @@ namespace AL::OS::OpenGL
 				"Context not created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
 			::GLfloat tl_X, tl_Y;
 			TranslatePositionToNative(position, tl_X, tl_Y);
 
@@ -363,9 +334,6 @@ namespace AL::OS::OpenGL
 			::glVertex2f(bl_X, bl_Y);
 
 			::glEnd();
-#else
-			throw NotImplementedException();
-#endif
 		}
 
 		// @throw AL::Exception
@@ -388,7 +356,6 @@ namespace AL::OS::OpenGL
 				"Context not created"
 			);
 
-#if defined(AL_DEPENDENCY_OPENGL)
 			::GLfloat start_X, start_Y;
 			TranslatePositionToNative(start, start_X, start_Y);
 
@@ -403,9 +370,6 @@ namespace AL::OS::OpenGL
 			::glVertex2f(end_X, end_Y);
 
 			::glEnd();
-#else
-			throw NotImplementedException();
-#endif
 		}
 
 		// @throw AL::Exception
