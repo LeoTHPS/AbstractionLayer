bfefa6489beea443846eaca3b52b02475cfe8537
Francis Barney <leo633395@gmail.com>
2023-02-15 21:30:17 -0800

    add AL::Hardware::Pico::Clock

---
+++ AL/Hardware/Pico/Clock.hpp
@@ -0,0 +1,126 @@
+#pragma once
+#include "AL/Common.hpp"
+
+#if !defined(AL_PLATFORM_RP2040)
+	#error Platform not supported
+#endif
+
+#include "GPIO.hpp"
+
+#include <hardware/clocks.h>
+
+namespace AL::Hardware::Pico
+{
+	enum class Clocks : Get_Enum_Or_Integer_Base<::clock_index>::Type
+	{
+		ADC        = ::clk_adc,
+		RTC        = ::clk_rtc,
+		USB        = ::clk_usb,
+		GPIO0      = ::clk_gpout0,
+		GPIO1      = ::clk_gpout1,
+		GPIO2      = ::clk_gpout2,
+		GPIO3      = ::clk_gpout3,
+		System     = ::clk_sys,
+		Reference  = ::clk_ref,
+		Peripheral = ::clk_peri
+	};
+
+	class Clock
+	{
+		inline static Bool isInitialized = False;
+
+		Clock() = delete;
+
+	public:
+		static Bool IsInitialized()
+		{
+			return isInitialized;
+		}
+
+		static Void Init()
+		{
+			if (!IsInitialized())
+			{
+				::clocks_init();
+
+				isInitialized = True;
+			}
+		}
+
+		static uint32 GetFrequency(Clocks clock)
+		{
+			return ::clock_get_hz(
+				static_cast<::clock_index>(clock)
+			);
+		}
+
+		// @param src The main clock source, can be 0
+		// @param aux_src The auxiliary clock source, which depends on which clock is being set. Can be 0
+		// @param src_freq Frequency of the input clock source
+		// @param freq Requested frequency
+		static Bool Configure(Clocks clock, uint32 src, uint32 aux_src, uint32 src_freq, uint32 freq)
+		{
+			if (!::clock_configure(static_cast<::clock_index>(clock), src, aux_src, src_freq, freq))
+			{
+
+				return False;
+			}
+
+			return True;
+		}
+
+		// @param pin The GPIO pin to output the clock to. Valid GPIOs are: 21, 23, 24, 25. These GPIOs are connected to the GPOUT0-3 clock generators
+		// @param src The source clock. See the register field CLOCKS_CLK_GPOUT0_CTRL_AUXSRC for a full list. The list is the same for each GPOUT clock generator
+		// @param div The amount to divide the source clock by. This is useful to not overwhelm the GPIO pin with a fast clock
+		static Void ConfigureGPIO(Clocks clock, GPIOPin pin, uint32 src, Float div)
+		{
+			::clock_gpio_init(
+				pin,
+				src,
+				div
+			);
+		}
+		// @param pin The GPIO pin to output the clock to. Valid GPIOs are: 21, 23, 24, 25. These GPIOs are connected to the GPOUT0-3 clock generators
+		// @param src The source clock. See the register field CLOCKS_CLK_GPOUT0_CTRL_AUXSRC for a full list. The list is the same for each GPOUT clock generator
+		// @param div_int The fractional part of the value to divide the source clock by. This is in range of 0..255 (/256).
+		// @param div_frac The integer part of the value to divide the source clock by. This is useful to not overwhelm the GPIO pin with a fast clock. this is in range of 1..2^24-1.
+		static Void ConfigureGPIO(Clocks clock, GPIOPin pin, uint32 src, uint32 div_int, uint32 div_frac)
+		{
+			::clock_gpio_init_int_frac(
+				pin,
+				src,
+				div_int,
+				div_frac
+			);
+		}
+
+		// @param pin The GPIO pin to run the clock from. Valid GPIOs are: 20 and 22
+		// @param src_freq Frequency of the input clock source
+		// @param freq Requested frequency
+		static Bool ConfigureGPIOInput(Clocks clock, GPIOPin pin, uint32 src_freq, uint32 freq)
+		{
+			if (!::clock_configure_gpin(static_cast<::clock_index>(clock), pin, src_freq, freq))
+			{
+
+				return False;
+			}
+
+			return True;
+		}
+
+		static Void Stop(Clocks clock)
+		{
+			::clock_stop(
+				static_cast<::clock_index>(clock)
+			);
+		}
+
+		static Void EnableGPIO(GPIOPin pin, Bool set = True)
+		{
+			::gpio_set_function(
+				pin,
+				set ? ::GPIO_FUNC_GPCK : ::GPIO_FUNC_NULL
+			);
+		}
+	};
+}
