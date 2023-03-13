6f3cd9b688bc861580db062a92091cfc45017e6e
Francis Barney <leo633395@gmail.com>
2023-03-13 14:04:01 -0700

    add AL::Hardware::Pico::Boot

---
+++ AL/Hardware/Pico/Boot.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "AL/Common.hpp"
+
+#if !defined(AL_PLATFORM_RP2040)
+	#error Platform not supported
+#endif
+
+#include <pico/bootrom.h>
+
+namespace AL::Hardware::Pico
+{
+	class Boot
+	{
+		Boot() = delete;
+
+	public:
+		static Void Reset()
+		{
+			::reset_usb_boot(
+				0,
+				0
+			);
+		}
+	};
+}
