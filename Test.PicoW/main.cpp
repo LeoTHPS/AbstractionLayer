0b789f4bb729cff24c16a2c271bc8dadfabc5f97
Francis Barney <leo633395@gmail.com>
2023-06-25 13:33:58 -0700

    update

--- Test.PicoW/main.cpp
+++ Test.PicoW/main.cpp
@@ -5,14 +5,14 @@
 
 #include <AL/OS/Pico/Stdio.hpp>
 
-#include <AL/Hardware/PicoW/CYW43.hpp>
+#include <AL/Hardware/Pico/CYW43.hpp>
 
 #define WIFI_DEFAULT_SSID      "ssid"
 #define WIFI_DEFAULT_PASSWORD  "passwd"
-#define WIFI_DEFAULT_AUTH_TYPE AL::Hardware::PicoW::CYW43AuthTypes::WPA2_AES
+#define WIFI_DEFAULT_AUTH_TYPE AL::Hardware::Pico::CYW43AuthTypes::WPA2_AES_PSK
 
 // @throw AL::Exception
-void wifi_scan(AL::Hardware::PicoW::CYW43Network& network, AL::Bool& networkFound)
+void wifi_scan(AL::Hardware::Pico::CYW43Network& network, AL::Bool& networkFound)
 {
 	AL::OS::Console::WriteLine(
 		"Scanning for open WiFi networks"
@@ -20,10 +20,10 @@ void wifi_scan(AL::Hardware::PicoW::CYW43Network& network, AL::Bool& networkFoun
 
 	networkFound = AL::False;
 
-	AL::Hardware::PicoW::CYW43ScanCallback onScan(
-		[&network, &networkFound](const AL::Hardware::PicoW::CYW43Network& _network)
+	AL::Hardware::Pico::CYW43ScanCallback onScan(
+		[&network, &networkFound](const AL::Hardware::Pico::CYW43Network& _network)
 		{
-			if (_network.AuthType == AL::Hardware::PicoW::CYW43AuthTypes::Open)
+			if (_network.AuthType == AL::Hardware::Pico::CYW43AuthTypes::Open)
 			{
 				AL::OS::Console::WriteLine(
 					"Found open network %s [BSSID: %s, Channel: %u, RSSI: %i]",
@@ -52,13 +52,13 @@ void wifi_scan(AL::Hardware::PicoW::CYW43Network& network, AL::Bool& networkFoun
 		}
 	);
 
-	AL::Hardware::PicoW::CYW43::Scan(
+	AL::Hardware::Pico::CYW43::Scan(
 		onScan
 	);
 }
 
 // @throw AL::Exception
-void wifi_connect(const AL::Hardware::PicoW::CYW43Network& network, AL::Bool networkFound)
+void wifi_connect(const AL::Hardware::Pico::CYW43Network& network, AL::Bool networkFound)
 {
 	AL::OS::Console::WriteLine(
 		"Connecting to %s",
@@ -69,13 +69,13 @@ void wifi_connect(const AL::Hardware::PicoW::CYW43Network& network, AL::Bool net
 	{
 		if (networkFound)
 		{
-			AL::Hardware::PicoW::CYW43::Connect(
+			AL::Hardware::Pico::CYW43::Connect(
 				network.SSID
 			);
 		}
 		else
 		{
-			AL::Hardware::PicoW::CYW43::Connect(
+			AL::Hardware::Pico::CYW43::Connect(
 				WIFI_DEFAULT_SSID,
 				WIFI_DEFAULT_PASSWORD,
 				WIFI_DEFAULT_AUTH_TYPE
@@ -94,27 +94,32 @@ void wifi_connect(const AL::Hardware::PicoW::CYW43Network& network, AL::Bool net
 void wifi_open()
 {
 	AL::OS::Console::WriteLine(
-		"Opening AL::Hardware::PicoW::CYW43"
+		"Opening AL::Hardware::Pico::CYW43"
 	);
 
-	AL::Hardware::PicoW::CYW43::Open(
-		AL::Hardware::PicoW::CYW43Countries::USA
+	AL::Hardware::Pico::CYW43::Open(
+		AL::Hardware::Pico::CYW43Countries::USA
 	);
 }
 
 void wifi_close()
 {
 	AL::OS::Console::WriteLine(
-		"Closing AL::Hardware::PicoW::CYW43"
+		"Closing AL::Hardware::Pico::CYW43"
 	);
 
-	AL::Hardware::PicoW::CYW43::Close();
+	AL::Hardware::Pico::CYW43::Close();
 }
 
 // @throw AL::Exception
 // @return false to shutdown
 bool do_the_thing_once(AL::TimeSpan delta)
 {
+	using namespace AL;
+	using namespace AL::OS;
+	using namespace AL::Hardware;
+
+
 
 	return true;
 }
@@ -122,26 +127,10 @@ bool do_the_thing_once(AL::TimeSpan delta)
 // @throw AL::Exception
 void do_the_thing()
 {
-	AL::OS::Pico::Stdio::Init();
-
-	for (int i = 0; i < 5; ++i)
-	{
-		AL::OS::Console::WriteLine(
-			"Starting in %i..",
-			5 - i
-		);
-
-		AL::Sleep(
-			AL::TimeSpan::FromSeconds(1)
-		);
-
-		AL::OS::Console::Clear();
-	}
-
 	wifi_open();
 
-	AL::Hardware::PicoW::CYW43Network network;
-	AL::Bool                          networkFound;
+	AL::Hardware::Pico::CYW43Network network;
+	AL::Bool                         networkFound;
 
 	wifi_scan(
 		network,
@@ -153,7 +142,7 @@ void do_the_thing()
 		networkFound
 	);
 
-	AL::Hardware::PicoW::CYW43::LED::Write(
+	AL::Hardware::Pico::CYW43::LED::Write(
 		AL::True
 	);
 
@@ -164,7 +153,7 @@ void do_the_thing()
 		);
 	}
 
-	AL::Hardware::PicoW::CYW43::LED::Write(
+	AL::Hardware::Pico::CYW43::LED::Write(
 		AL::False
 	);
 
@@ -173,25 +162,29 @@ void do_the_thing()
 
 int main()
 {
+	AL::OS::Pico::Stdio::Init();
+
+#if defined(LIB_PICO_STDIO_USB)
+	AL::OS::Pico::Stdio::SetTranslateCRLF(stdio_usb);
+
+	while (!AL::OS::Pico::Stdio::IsUsbConnected())
+	{
+	}
+#elif defined(LIB_PICO_STDIO_UART)
+	AL::OS::Pico::Stdio::SetTranslateCRLF(stdio_uart);
+#elif defined(LIB_PICO_STDIO_SEMIHOSTING)
+	AL::OS::Pico::Stdio::SetTranslateCRLF(stdio_semihosting);
+#endif
+
 	try
 	{
 		do_the_thing();
 	}
 	catch (const AL::Exception& exception)
 	{
-		AL::OS::Console::WriteLine(
-			exception.GetMessage()
+		AL::OS::Console::WriteException(
+			exception
 		);
-
-		if (auto lpInnerException = exception.GetInnerException())
-		{
-			do
-			{
-				AL::OS::Console::WriteLine(
-					lpInnerException->GetMessage()
-				);
-			} while ((lpInnerException = lpInnerException->GetInnerException()) != nullptr);
-		}
 	}
 
 	AL::Sleep(
