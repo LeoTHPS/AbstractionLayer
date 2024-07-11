a0b0fce97f7845743c082025760c114ab3bb895e
Francis Barney <leo633395@gmail.com>
2024-07-11 12:23:23 -0700

    fix for pico_w builds

--- AL/Hardware/Pico/Pins.hpp
+++ AL/Hardware/Pico/Pins.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include "AL/Common.hpp"
 
-#if defined(AL_PLATFORM_PICO)
-	#include <boards/pico.h>
-#elif defined(AL_PLATFORM_PICO_W)
+#if defined(AL_PLATFORM_PICO_W)
 	#include <boards/pico_w.h>
+#elif defined(AL_PLATFORM_PICO)
+	#include <boards/pico.h>
 #else
 	#error Platform not supported
 #endif
@@ -45,7 +45,18 @@ namespace AL::Hardware::Pico
 
 		PINS_COUNT,
 
-#if defined(AL_PLATFORM_PICO)
+#if defined(AL_PLATFORM_PICO_W)
+		PINS_UART_RX         = PICO_DEFAULT_UART_RX_PIN,
+		PINS_UART_TX         = PICO_DEFAULT_UART_TX_PIN,
+
+		PINS_I2C_SCL         = PICO_DEFAULT_I2C_SCL_PIN,
+		PINS_I2C_SDA         = PICO_DEFAULT_I2C_SDA_PIN,
+
+		PINS_SPI_RX          = PICO_DEFAULT_SPI_RX_PIN,
+		PINS_SPI_TX          = PICO_DEFAULT_SPI_TX_PIN,
+		PINS_SPI_CS          = PICO_DEFAULT_SPI_CSN_PIN,
+		PINS_SPI_SCLK        = PICO_DEFAULT_SPI_SCK_PIN
+#elif defined(AL_PLATFORM_PICO)
 		PINS_LED             = PICO_DEFAULT_LED_PIN,
 
 		PINS_SMPS            = PICO_SMPS_MODE_PIN,
@@ -68,17 +79,6 @@ namespace AL::Hardware::Pico
 		PINS_CLOCK_OUTPUT_23 = PINS_GPIO_23,
 		PINS_CLOCK_OUTPUT_24 = PINS_GPIO_24,
 		PINS_CLOCK_OUTPUT_25 = PINS_GPIO_25
-#elif defined(AL_PLATFORM_PICO_W)
-		PINS_UART_RX         = PICO_DEFAULT_UART_RX_PIN,
-		PINS_UART_TX         = PICO_DEFAULT_UART_TX_PIN,
-
-		PINS_I2C_SCL         = PICO_DEFAULT_I2C_SCL_PIN,
-		PINS_I2C_SDA         = PICO_DEFAULT_I2C_SDA_PIN,
-
-		PINS_SPI_RX          = PICO_DEFAULT_SPI_RX_PIN,
-		PINS_SPI_TX          = PICO_DEFAULT_SPI_TX_PIN,
-		PINS_SPI_CS          = PICO_DEFAULT_SPI_CSN_PIN,
-		PINS_SPI_SCLK        = PICO_DEFAULT_SPI_SCK_PIN
 #endif
 	};
 }
